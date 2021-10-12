df5f66da4bf62869205436efaafce0a4c762f043
Miles Zimmerman <miles@houseninja.co>
2021-10-11 19:27:42 -0700

    first build attempt

---
+++ app/routes/index.js
@@ -0,0 +1,11 @@
+import Route from '@ember/routing/route';
+// import { Dialog } from '@capacitor/dialog';
+
+export default class IndexRoute extends Route {
+  // async model() {
+  //   await Dialog.alert({
+  //     title: 'Hello',
+  //     message: 'World!',
+  //   });
+  // }
+}
