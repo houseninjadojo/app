c3423fa1846e10d101934d0795316b436d1b2504
Miles Zimmerman <mileszim@protonmail.com>
2021-11-04 01:27:13 -0700

    feat: add click tracking to user analytics (#18)

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,12 +5,17 @@ export default class ApplicationRoute extends Route {
   @service session;
   @service router;
   @service analytics;
+  @service('ember-user-activity@user-activity') userActivity;
 
   constructor() {
     super(...arguments);
 
     this.router.on('routeDidChange', async () => {
       await this._trackPage();
+    });
+
+    this.userActivity.on('touchstart', this, async (event) => {
+      await this._trackClick(event);
     });
   }
 
@@ -30,5 +35,20 @@ export default class ApplicationRoute extends Route {
     const page = this.router.currentURL;
     const title = this.router.currentRouteName || 'unknown';
     await this.analytics.track('page_visit', { page, title });
+  }
+
+  /**
+   * For every click or touchevent, generate a query selector
+   * from the triggering DOM element and create an analytics event.
+   * <div id="a" class="b c"></div> => `div.b.c#a`
+   */
+  async _trackClick(event) {
+    const tag = event.target.localName;
+    const classNames = event.target.className.replaceAll(' ', '.');
+    const id = event.target.id;
+    const queryString = `${tag}.${classNames}${id.length > 0 ? '#' + id : ''}`;
+    await this.analytics.track('click', {
+      selector: queryString,
+    });
   }
 }
