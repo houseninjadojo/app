2be65ccaac075949341f4a9e10cce918e57f06e3
Miles Zimmerman <miles@houseninja.co>
2021-11-03 23:58:38 -0700

    Update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,19 @@
 
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
   parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
-    ecmaFeatures: {
-      legacyDecorators: true,
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        ['@babel/plugin-proposal-decorators', { decoratorsBeforeExport: true }],
+      ],
     },
+    // ecmaFeatures: {
+    //   legacyDecorators: true,
+    // },
   },
   plugins: ['ember'],
   extends: [
