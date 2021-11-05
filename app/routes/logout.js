395eb332acfdc3dfa411942b57e5c2c72bdb388d
Miles Zimmerman <miles@houseninja.co>
2021-11-05 15:22:18 -0700

    fix: logout in external auth provider in addition to token invalidation

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,5 +1,7 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { Browser } from '@capacitor/browser';
+import { getOwner } from '@ember/application';
 
 export default class LogoutRoute extends Route {
   @service session;
@@ -8,8 +10,21 @@ export default class LogoutRoute extends Route {
 
   async beforeModel() {
     await this.analytics.track('logout');
+    await this.logoutExternal();
     await this.session.invalidate();
     await this.analytics.reset();
     this.router.transitionTo('index');
+  }
+
+  async logoutExternal() {
+    Browser.addListener('browserPageLoaded', () => {
+      Browser.close();
+    });
+    await Browser.open({ url: this.logoutUrl(), presentationStyle: 'popover' });
+  }
+
+  logoutUrl() {
+    const pkce = getOwner(this).lookup('authenticator:pkce');
+    return pkce.logoutEndpoint;
   }
 }
