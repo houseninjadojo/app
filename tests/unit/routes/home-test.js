04c0c032be19ace5b34f57b57f75828808f2a0c0
achillesimperial <hn@ls.local>
2021-12-01 14:35:25 -0500

    Renamed Dashboard to Home and updated tests

--- tests/unit/routes/home-test.js
+++ tests/unit/routes/home-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | dashboard', function (hooks) {
+module('Unit | Route | home', function (hooks) {
   setupTest(hooks);
 
   test('it exists', function (assert) {
-    let route = this.owner.lookup('route:dashboard');
+    let route = this.owner.lookup('route:home');
     assert.ok(route);
   });
 });
