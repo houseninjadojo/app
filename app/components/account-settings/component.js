da99604584d922917369042ad0d5824e536d3c80
achillesimperial <hn@ls.local>
2021-12-02 16:49:42 -0500

    Account Settings View Menu

---
+++ app/components/account-settings/component.js
@@ -0,0 +1,53 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object';
+
+export default class AccountSettingsComponent extends Component {
+  @service router;
+
+  /**
+   * The current route selection
+   */
+    
+  menuItems = [
+    {
+        label: 'Home',
+        name: 'home',
+    },
+    {
+        label: 'Contact Information',
+        name: 'contact',
+    },
+    {
+        label: 'Payment Method',
+        name: 'payment',
+    },
+    {
+        label: 'Security Settings',
+        name: 'security',
+    },
+    {        
+        label: 'Property Details',
+        name: 'property',
+    },
+    {
+        label: 'Contact Us',
+        name: 'contact',
+    },
+    {
+        label: 'FAQ',
+        name: 'faq',
+    },
+    {
+        label: 'Logout',
+        name: 'logout',
+    },
+  ];
+
+  @action
+  selectRoute(routeName) {
+    console.log(routeName);
+    // this.selected = routeName;
+    // this.router.transitionTo(routeName);
+  }
+}
