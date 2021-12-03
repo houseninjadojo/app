f9c18416045d7a052b1ad8bb7c6cb61a041f7eed
achillesimperial <hn@ls.local>
2021-12-03 15:11:49 -0500

    Prelim Payment component -- need to update inputs with Select/Dropdown

---
+++ app/components/account-settings/payment/component.js
@@ -0,0 +1,32 @@
+import Component from '@glimmer/component';
+
+export default class ContactComponent extends Component {
+  
+  contactFields = [
+    {
+        required: true,
+        label: 'Card Number',
+        placeholder: '',
+    },
+    {
+        required: true,
+        label: 'CVC',
+        placeholder: '',
+    },
+    {
+        required: true,
+        label: 'Month',
+        placeholder: 'MM',        
+    },
+    {
+      required: true,
+      label: 'Year',
+      placeholder: 'YYYY',      
+    },
+    {
+        required: true,
+        label: 'Zipcode',
+        placeholder: '',
+    },    
+  ]
+}
