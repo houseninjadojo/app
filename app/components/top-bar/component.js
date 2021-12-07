e77a93f2d9f01b0e84d5bab24a275be04c5e5709
achillesimperial <hn@ls.local>
2021-12-07 17:01:19 -0500

    Modified TopBar component

---
+++ app/components/top-bar/component.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class NavBarComponent extends Component {  
+  @tracked modalVisible = false;
+  
+  @action
+  toggleSettingsViewVisibility() {
+    console.log('Toggle Settings Modal Visibility');
+  }
+}
