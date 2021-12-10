217c69b8d2004efc306b060b42172192eec47fa7
Miles Zimmerman <mileszim@protonmail.com>
2021-12-10 15:21:17 -0800

    fix: hamburger opens settings page (#102)

--- app/components/top-bar/component.js
+++ app/components/top-bar/component.js
@@ -1,12 +1,15 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class NavBarComponent extends Component {
+  @service router;
   @tracked modalVisible = false;
 
   @action
   toggleSettingsViewVisibility() {
     console.log('Toggle Settings Modal Visibility');
+    this.router.transitionTo('settings');
   }
 }
