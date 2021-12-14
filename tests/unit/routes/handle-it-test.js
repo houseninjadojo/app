4011dae74de10b35c6580f268fe37fcd20770af6
Miles Zimmerman <miles@houseninja.co>
2021-12-13 17:46:16 -0800

    fix lint

--- tests/unit/routes/handle-it-test.js
+++ tests/unit/routes/handle-it-test.js
@@ -1,10 +1,10 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | handle-it', function(hooks) {
+module('Unit | Route | handle-it', function (hooks) {
   setupTest(hooks);
 
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let route = this.owner.lookup('route:handle-it');
     assert.ok(route);
   });
