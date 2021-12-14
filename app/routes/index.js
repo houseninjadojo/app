d8a8da865c286af1f52983ce8db2d0a82887a009
Miles Zimmerman <miles@houseninja.co>
2021-12-13 19:35:14 -0800

    remove index model test

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
 
 export default class IndexRoute extends Route {
   @service session;
-  @service store;
-
-  model() {
-    return this.store.findAll('home', { include: 'address' });
-  }
 }
