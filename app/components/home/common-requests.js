ea274469d981fae502aa639e8321426318c76cfc
achillesimperial <hn@ls.local>
2021-12-15 10:18:37 -0500

    Created and added CommonRequests component

---
+++ app/components/home/common-requests.js
@@ -0,0 +1,63 @@
+import Component from '@glimmer/component';
+
+const baseURI = 'assets/images/common-requests';
+
+export default class CommonRequestsComponent extends Component {  
+  commonRequests = [
+    {
+      caption: 'Request Plumbing Service',
+      imgURI: `${baseURI}/marian-florinel-condruz-C-oYJoIfgCs-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request A/C Repair Service',
+      imgURI: `${baseURI}/raychan-KGqif07slUY-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request Appliance Repair',
+      imgURI: `${baseURI}/kam-idris-Ot2iTXgC6fY-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Schedule Outdoor Cleaning',
+      imgURI: `${baseURI}/daniel-watson-8vBpYpTGo90-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request Lighting Upgrade',
+      imgURI: `${baseURI}/etienne-girardet-NGb91VwnOWY-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request Custom Storage Solution',
+      imgURI: `${baseURI}/pickawood-LnXm8xLCFtg-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request GFCI Outlet Fix',
+      imgURI: `${baseURI}/clint-patterson-exfrR9KkzlE-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },  
+    {
+      caption: 'Request Painting',
+      imgURI: `${baseURI}/theme-photos-Cl-OpYWFFm0-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request Carpet/Upholstery Cleaning',
+      imgURI: `${baseURI}/the-creative-exchange-uSFYHjmqPAE-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request BBQ Grill Cleaning',
+      imgURI: `${baseURI}/adam-mills-SHFQI_DGgAU-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+    {
+      caption: 'Request Chimney Cleaning',
+      imgURI: `${baseURI}/vlado-paunovic-u1Q3Aem_TWc-unsplash.jpg`,
+      defaultHNChatMessage: '',
+    },
+  ]
+}
