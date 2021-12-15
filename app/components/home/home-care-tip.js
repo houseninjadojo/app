82c6091d8e4de0d1d673c094297f7cbc0cba46a1
achillesimperial <hn@ls.local>
2021-12-15 10:20:26 -0500

    Created and added HomeCareTip component

---
+++ app/components/home/home-care-tip.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+
+export default class HomeCareTipComponent extends Component {  
+  homeCareTips = [
+    {
+      label: 'Tip Label',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec leo felis. Cras auctor at risus non blandit. Vivamus mattis vel nisl nec varius. Etiam sapien velit, cursus eu lacus nec, accumsan convallis ex. Praesent sit amet maximus dui.',
+      showButton: true,
+      defaultHNChatMessage: '',
+    }
+  ]
+}
