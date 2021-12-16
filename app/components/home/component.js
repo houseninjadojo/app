1f7225fc4171aca64cf62ebfa64a189706d1f0c4
achillesimperial <hn@ls.local>
2021-12-16 10:32:56 -0500

    Fixed and refactored HomeComponent

--- app/components/home/component.js
+++ app/components/home/component.js
@@ -1,13 +1,16 @@
 import Component from '@glimmer/component';
-@service current;
+import { inject as service } from '@ember/service';
 
 export default class HomeContentComponent extends Component {
-  get user() {
+  @service current;
+
+  get getUser() {
     return this.current.user;
   }
 
-  get property() {
-    return this.current.property;
+  get getProperty() {
+    const streetAddress = this.current.property.content.line1;
+    return streetAddress || 'home';
   }
 
   get getSalutation() {
@@ -16,17 +19,14 @@ export default class HomeContentComponent extends Component {
 
     let greet;
 
-    if (hrs < 12)
-      greet = 'Good Morning';
-    else if (hrs >= 12 && hrs <= 17)
-      greet = 'Good Afternoon';
-    else if (hrs >= 17 && hrs <= 24)
-      greet = 'Good Evening';
+    if (hrs < 12) greet = 'Good Morning';
+    else if (hrs >= 12 && hrs <= 17) greet = 'Good Afternoon';
+    else if (hrs >= 17 && hrs <= 24) greet = 'Good Evening';
 
-    return `${greet}, ${this.user.firstName}!`;
+    return `${greet}, ${this.getUser.firstName}!`;
   }
 
   get getPrompt() {
-    return `What can we help you with at ${this.property.line1}?`;
+    return `What can we help you with at ${this.getProperty}?`;
   }
 }
