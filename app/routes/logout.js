0725a0d6762aa5884cc414415a01825e5acbe754
Miles Zimmerman <miles@houseninja.co>
2021-12-29 13:27:24 -0800

    RFC-0752: replace  with just

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
+import { service } from '@ember/service';
 import { Browser } from '@capacitor/browser';
 import { getOwner } from '@ember/application';
 
