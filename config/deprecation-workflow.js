494d8bad267ff73b6e1b2cdb073d85393f215ddd
Miles Zimmerman <miles@houseninja.co>
2021-12-29 12:37:01 -0800

    test(ember): add deprecation silencer for ember tests

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,9 @@
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-global" },
+    { handler: "silence", matchId: "ember-simple-auth.initializer.setup-session-restoration" },
+    { handler: "silence", matchId: "deprecated-run-loop-and-computed-dot-access" },
+    { handler: "silence", matchId: "ember-data:default-serializer" }
+  ]
+};
--- mirage/factories/property.js
+++ mirage/factories/property.js
@@ -5,35 +5,35 @@ export default Factory.extend({
   address: association(),
 
   lotSize() {
-    return faker.random.number({ min: 10000, max: 100000 });
+    return faker.datatype.number({ min: 10000, max: 100000 });
   },
 
   homeSize() {
-    return faker.random.number({ min: 1000, max: 10000 });
+    return faker.datatype.number({ min: 1000, max: 10000 });
   },
 
   garageSize() {
-    return faker.random.number({ min: 0, max: 2 });
+    return faker.datatype.number({ min: 0, max: 2 });
   },
 
   homeAge() {
-    return faker.random.number({ min: 1, max: 100 });
+    return faker.datatype.number({ min: 1, max: 100 });
   },
 
   estimatedValue() {
-    return faker.random.number({ min: 500000, max: 15000000 });
+    return faker.datatype.number({ min: 500000, max: 15000000 });
   },
 
   bedrooms() {
-    return faker.random.number({ min: 1, max: 10 });
+    return faker.datatype.number({ min: 1, max: 10 });
   },
 
   bathrooms() {
-    return faker.random.number({ min: 1, max: 10 });
+    return faker.datatype.number({ min: 1, max: 10 });
   },
 
   pools() {
-    return faker.random.number({ min: 0, max: 1 });
+    return faker.datatype.number({ min: 0, max: 1 });
   },
 
   default: true,
