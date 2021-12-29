6bb67f61ef41e84e2190a087495d51dd5019aa6d
Miles Zimmerman <mileszim@protonmail.com>
2021-12-29 12:46:05 -0800

    test(ember): add deprecation silencer for ember tests (#155)
    * test(ember): add deprecation silencer for ember tests
    
    * remove deprecation-workflow.js from eslint

---
+++ config/deprecation-workflow.js
@@ -0,0 +1,10 @@
+// @see https://github.com/mixonic/ember-cli-deprecation-workflow
+self.deprecationWorkflow = self.deprecationWorkflow || {};
+self.deprecationWorkflow.config = {
+  workflow: [
+    { handler: "silence", matchId: "ember-global" },
+    { handler: "silence", matchId: "ember-simple-auth.initializer.setup-session-restoration" },
+    { handler: "silence", matchId: "deprecated-run-loop-and-computed-dot-access" },
+    { handler: "silence", matchId: "ember-data:default-serializer" }
+  ]
+};
