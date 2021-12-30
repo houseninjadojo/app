f2f495e85f8ed7251ec4e41a618e4b609bcc3104
Miles Zimmerman <miles@houseninja.co>
2021-12-30 13:16:32 -0800

    fix lint

--- tests/unit/utils/haptics-test.js
+++ tests/unit/utils/haptics-test.js
@@ -1,20 +1,19 @@
-import {
-  impact,
-  notification,
-  vibrate,
-  selectionStart,
-  selectionChanged,
-  selectionEnd,
-} from 'houseninja/utils/haptics';
+// import {
+//   impact,
+//   notification,
+//   vibrate,
+//   selectionStart,
+//   selectionChanged,
+//   selectionEnd,
+// } from 'houseninja/utils/haptics';
 import { module, skip } from 'qunit';
-import sinon from 'sinon';
+// import sinon from 'sinon';
 
 import { Haptics } from '@capacitor/haptics';
 
 // @todo I don't know how to spy on Proxy objects
 // If that is figured out then maybe we can test it
 module('Unit | Utility | haptics', function () {
-
   // hooks.beforeEach(function () {
   //   console.log(HapticsPlugin);
   //   sinon.stub(HapticsPlugin, 'impact').return(true);
