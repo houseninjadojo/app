1375ddd323da21ccfdc358c76a1a3df0fbc8651f
Miles Zimmerman <miles@houseninja.co>
2021-12-30 21:32:31 -0800

    being fastidious

--- app/models/user.js
+++ app/models/user.js
@@ -2,8 +2,8 @@ import Model, { attr, hasMany } from '@ember-data/model';
 
 export default class UserModel extends Model {
   @hasMany('devices') devices;
-  @hasMany('property') properties;
   @hasMany('payment-method', { polymorphic: true }) paymentMethods;
+  @hasMany('property') properties;
 
   @attr('string') email;
   @attr('string') firstName;
