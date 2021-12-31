36584ac554445af188d3848936eb750093d1a076
Miles Zimmerman <miles@houseninja.co>
2021-12-30 17:15:35 -0800

    stash

--- app/models/notification.js
+++
@@ -1,5 +0,0 @@
-import { get, set } from '@ember/object';
-
-export default class Notification {
-
-}
--- app/utils/native/local-notifications.js
+++ app/utils/native/local-notifications.js
@@ -5,11 +5,50 @@ import { debug } from '@ember/debug';
 /**
  * Schedule one or more local notifications.
  *
- * @param {Object} options - ScheduleOptions
- * @param {Array<Object>} [options.notifications] LocalNotificationSchema[]
+ * @param {Object[]} notifications LocalNotificationSchema[]
+ * @return {Object[]} LocalNotificationDescriptor[]
  */
-export async function schedule(options) {
+export async function schedule(notifications) {
+  notifications = [notifications].flat();
   return await run(async () => {
-    return await LocalNotifications.schedule(options);
+    let result = await LocalNotifications.schedule({ notifications });
+    return result.notifications;
+  });
+}
+
+/**
+ * Get a list of pending notifications.
+ *
+ * @return {Object[]}  PendingLocalNotificationSchema[]
+ */
+export async function getPending() {
+  return await run(async () => {
+    let result = await LocalNotifications.getPending();
+    return result.notifications;
+  });
+}
+
+/**
+ * Register actions to take when notifications are displayed.
+ * Only available for iOS and Android.
+ *
+ * @param {Object} options - 	RegisterActionTypesOptions
+ * @param {Object[]} options.types 	ActionType[]
+ */
+export async function registerActionTypes(options) {
+  return await run(async () => {
+    return await LocalNotifications.registerActionTypes(options);
+  });
+}
+
+/**
+ * Cancel pending notifications.
+ *
+ * @param {Object} options CancelOptions
+ * @param {Object[]} options.notifications LocalNotificationDescriptor[]
+ */
+export async function cancel(options) {
+  return await run(async () => {
+    return await LocalNotifications.cancel(options);
   });
 }
