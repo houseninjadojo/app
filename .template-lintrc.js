108ab013e2a61db1fd839d2e3f0729d917ca3a76
achillesimperial <achilles@houseninja.co>
2022-01-04 17:00:18 -0500

    PR feedback implementation

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,8 +2,4 @@
 
 module.exports = {
   extends: 'recommended',
-  rules: {
-    'no-curly-component-invocation': { allow: ['liquid-outlet'] },
-    'no-implicit-this': { allow: ['liquid-outlet'] },
-  },
 };
