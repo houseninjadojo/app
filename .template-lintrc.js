a2fce93634c897b97df6d9a550f7954553b2cf99
achillesimperial <achilles@houseninja.co>
2022-01-04 15:45:05 -0500

    Replace {{outlet}} with {{liquid-outlet}} + ignoring linting associated linting errors

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -2,4 +2,8 @@
 
 module.exports = {
   extends: 'recommended',
+  rules: {
+    'no-curly-component-invocation': { allow: ['liquid-outlet'] },
+    'no-implicit-this': { allow: ['liquid-outlet'] },
+  },
 };
