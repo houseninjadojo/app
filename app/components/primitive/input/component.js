587f0f67de6f01a6461f6ac15a520d99a7594830
achillesimperial <achilles@houseninja.co>
2022-01-11 14:09:49 -0500

    Error/Warn handling for Input fields

--- app/components/primitive/input/component.js
+++ app/components/primitive/input/component.js
@@ -1,4 +1,22 @@
 import Component from '@glimmer/component';
 
-// eslint-disable-next-line
-export default class InputComponent extends Component {}
+export default class InputComponent extends Component {
+  get descriptionText() {
+    const alert = this.args.alert;
+    const warn = this.args.warn;
+    let description = this.args.description;
+    var descriptionString = '';
+
+    if (alert || warn) {
+      description = alert || warn;
+      if (description.title && description.detail) {
+        descriptionString += `${description.title} - ${description.detail}`;
+      } else {
+        descriptionString += description.title || description.detail;
+      }
+    } else if (description) {
+      descriptionString = description;
+    }
+    return descriptionString;
+  }
+}
