450a574ba924606951b622216237e54a3fa1eaf4
Achilles Imperial <achilles@houseninja.co>
2022-01-11 18:32:03 -0500

    Ai more cleaning (#195)
    * Added disabled prop to account settings form inputs
    
    * Added Select component to signup/property form
    
    * Style adjustments to primitive input and select
    
    * Set default city to Austin in property info of sign-up
    
    * Max width on hero container of sign-up
    
    * Removing up/down arrows for input[type=number]
    
    * Input container Warn and Error styles
    
    * Reordered input field semantic styles
    
    * Error/Warn handling for Input fields
    
    * Minor refactor
    
    * Modal container css fix
    
    * updated app icon assets (#196)

--- app/components/primitive/input/component.js
+++ app/components/primitive/input/component.js
@@ -1,4 +1,20 @@
 import Component from '@glimmer/component';
 
-// eslint-disable-next-line
-export default class InputComponent extends Component {}
+export default class InputComponent extends Component {
+  get descriptionText() {
+    const { alert, warn, description } = this.args;
+    const obj = alert || warn;
+    let descriptionString = '';
+
+    if (obj) {
+      if (obj.title && obj.detail) {
+        descriptionString += `${obj.title} - ${obj.detail}`;
+      } else {
+        descriptionString += obj.title || obj.detail;
+      }
+    } else if (description) {
+      descriptionString = description;
+    }
+    return descriptionString;
+  }
+}
