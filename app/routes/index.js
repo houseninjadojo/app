cdebd2ddd19187e2b16bd8d140b9cee8b681129f
Achilles Imperial <achilles@houseninja.co>
2022-01-13 15:46:37 -0500

    Misc ai (#197)
    * Set link color across app
    
    * SignUp/Payment checkbox and promocode inputs, need to update agreements link ref
    
    * SignUp or Login view/route based on Capacitor.isNativePlatform()

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,4 @@
+import { Capacitor } from '@capacitor/core';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
@@ -6,8 +7,10 @@ export default class IndexRoute extends Route {
   @service router;
 
   beforeModel() {
-    if (this.session.isAuthenticated) {
-      this.router.transitionTo('home');
+    if (Capacitor.isNativePlatform()) {
+      this.session.isAuthenticated
+        ? this.router.transitionTo('home')
+        : this.router.transitionTo('login-or-signup');
     } else {
       this.router.transitionTo('signup');
     }
