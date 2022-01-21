99efb23ebc5ac95eb8c039b5a2c6e2ec168ed809
Achilles Imperial <achilles@houseninja.co>
2022-01-21 15:05:45 -0500

    Updated login callback to route to home (#220)
    * Updated login callback to route to home
    
    * Added re-added Capactor import

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,13 @@
+import { Capacitor } from '@capacitor/core';
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-
-import isNativePlatform from 'houseninja/utils/is-native-platform';
-
 export default class IndexRoute extends Route {
   @service session;
   @service router;
 
   beforeModel() {
-    if (isNativePlatform()) {
-      this.session.isAuthenticated
-        ? this.router.transitionTo('dashboard.home')
-        : this.router.transitionTo('login-or-signup');
+    if (Capacitor.isNativePlatform()) {
+      this.router.transitionTo('login-or-signup');
     } else {
       this.router.transitionTo('signup');
     }
