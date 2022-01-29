da5657acdc8587d14dc0e66ab68ce668ece1862e
Miles Zimmerman <miles@houseninja.co>
2022-01-29 13:49:50 -0800

    lint and tests

--- tests/unit/utils/signup/breadcrumbs-test.js
+++ tests/unit/utils/signup/breadcrumbs-test.js
@@ -1,10 +1,10 @@
-import signupBreadcrumbs from 'houseninja/utils/signup/breadcrumbs';
+import breadcrumbs from 'houseninja/utils/signup/breadcrumbs';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | signup/breadcrumbs', function () {
   // TODO: Replace this with your real tests.
   test('it works', function (assert) {
-    let result = signupBreadcrumbs();
+    let result = breadcrumbs;
     assert.ok(result);
   });
 });
