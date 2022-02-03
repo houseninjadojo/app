d6e3b810609ac06acf5274bde180c4f7ce656f3e
Miles Zimmerman <miles@houseninja.co>
2022-02-03 13:03:14 -0800

    fix: signup/set-password submission

--- mirage/serializers/application.js
+++ mirage/serializers/application.js
@@ -2,6 +2,12 @@ import { JSONAPISerializer } from 'miragejs';
 import { underscore } from '@ember/string';
 
 export default JSONAPISerializer.extend({
+  /**
+   * Map dasherized keys to underscore
+   *
+   * @example
+   *   { "hello-world": "Hello World" } => { "hello_world": "Hello World" }
+   */
   keyForAttribute(attr) {
     return underscore(attr);
   },
