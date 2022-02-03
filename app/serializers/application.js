1a567205d5268b4ce84102cd029f4d3e420d810d
Miles Zimmerman <mileszim@protonmail.com>
2022-02-02 23:54:19 -0800

    chore: sandbox release prep (#274)
    * chore: sandbox release prep
    
    * what a mess

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,8 +1,8 @@
 import JSONAPISerializer from '@ember-data/serializer/json-api';
-// import { underscore } from '@ember/string';
+import { underscore } from '@ember/string';
 
 export default class ApplicationSerializer extends JSONAPISerializer {
-  // keyForAttribute(attr /* , method */) {
-  //   return underscore(attr);
-  // }
+  keyForAttribute(attr /* , method */) {
+    return underscore(attr);
+  }
 }
