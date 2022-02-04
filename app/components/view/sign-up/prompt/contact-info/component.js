a3350f968a41b12d3ffad1e38f2746ea7e480359
achillesimperial <achilles@houseninja.co>
2022-02-04 11:37:19 -0500

    Simplified PHONE_REGGIE

--- app/components/view/sign-up/prompt/contact-info/component.js
+++ app/components/view/sign-up/prompt/contact-info/component.js
@@ -77,7 +77,7 @@ export default class ContactInfoComponent extends Component {
     this.contactInfo[e.target.id] = e.target.value;
     const EMAIL_REGGIE =
       /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
-    const PHONE_REGGIE = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    const PHONE_REGGIE = /^(\d{3})-?(\d{3})-(\d{4})$/;
     this.requirementsModel = {
       noEmptyFields: Object.values(this.contactInfo).indexOf(false) === -1,
       emailIsValid: EMAIL_REGGIE.test(this.contactInfo.email),
