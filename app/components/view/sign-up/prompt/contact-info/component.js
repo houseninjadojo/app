1f45fe6bb62d8adb90e6e0eb8d2288ed66b47830
achillesimperial <achilles@houseninja.co>
2022-02-04 09:31:43 -0500

    signup contact-info form validation

--- app/components/view/sign-up/prompt/contact-info/component.js
+++ app/components/view/sign-up/prompt/contact-info/component.js
@@ -17,16 +17,18 @@ export default class ContactInfoComponent extends Component {
     email: null,
   };
 
+  @tracked formIsInvalid = true;
+
   fields = [
     {
-      id: 'first-name',
+      id: 'firstName',
       required: true,
       label: 'First Name',
       placeholder: '',
       value: 'firstName',
     },
     {
-      id: 'last-name',
+      id: 'lastName',
       required: true,
       label: 'Last Name',
       placeholder: '',
@@ -34,10 +36,10 @@ export default class ContactInfoComponent extends Component {
     },
     {
       type: 'tel',
-      id: 'phone',
+      id: 'phoneNumber',
       required: true,
       label: 'Phone',
-      placeholder: '',
+      placeholder: 'XXX-XXX-XXXX',
       description: 'We only use your phone number to contact you.',
       value: 'phoneNumber',
     },
@@ -68,5 +70,27 @@ export default class ContactInfoComponent extends Component {
   @action
   goBack() {
     this.router.transitionTo('signup.contact-info');
+  }
+
+  @action
+  validateForm(e) {
+    this.contactInfo[e.target.id] = e.target.value;
+    const EMAIL_REGGIE =
+      /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+    const PHONE_REGGIE = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    this.requirementsModel = {
+      noEmptyFields: Object.values(this.contactInfo).indexOf(false) === -1,
+      emailIsValid: EMAIL_REGGIE.test(this.contactInfo.email),
+      validPhonePattern: PHONE_REGGIE.test(this.contactInfo.phoneNumber),
+    };
+
+    const allRequirementsMet =
+      Object.values(this.requirementsModel).indexOf(false) === -1;
+
+    if (allRequirementsMet) {
+      this.formIsInvalid = false;
+    } else {
+      this.formIsInvalid = true;
+    }
   }
 }
