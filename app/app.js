ef3d9ecd1dd484de632c85425f8ee4f0b9c01a1f
Miles Zimmerman <miles@houseninja.co>
2022-02-10 19:36:14 -0800

    try this

--- app/app.js
+++ app/app.js
@@ -2,9 +2,21 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'houseninja/config/environment';
+import { InitSentryForEmber } from '@sentry/ember';
+import { BrowserTracing } from '@sentry/tracing';
 
-import { init } from 'houseninja/utils/sentry';
-init();
+const sentryOptions = config['@sentry/ember'].sentry;
+
+InitSentryForEmber({
+  ...sentryOptions,
+  integrations: [
+    new BrowserTracing({
+      tracingOrigins: ['api.houseninja.co', 'sandbox.api.houseninja.co'],
+    }),
+  ],
+});
+// import { init } from 'houseninja/utils/sentry';
+// init();
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
