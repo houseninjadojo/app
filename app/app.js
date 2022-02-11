e660607335f1c6c42d2ca3ce6f2555f15be71f84
Miles Zimmerman <miles@houseninja.co>
2022-02-10 20:01:58 -0800

    i think i get it

--- app/app.js
+++ app/app.js
@@ -2,21 +2,10 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'houseninja/config/environment';
-import { InitSentryForEmber } from '@sentry/ember';
-import { BrowserTracing } from '@sentry/tracing';
+import { init } from 'houseninja/utils/sentry';
 
-const sentryOptions = config['@sentry/ember'].sentry;
-
-InitSentryForEmber({
-  ...sentryOptions,
-  integrations: [
-    new BrowserTracing({
-      tracingOrigins: ['api.houseninja.co', 'sandbox.api.houseninja.co'],
-    }),
-  ],
-});
-// import { init } from 'houseninja/utils/sentry';
-// init();
+// init sentry
+init();
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
--- app/utils/sentry.js
+++ app/utils/sentry.js
@@ -1,39 +1,18 @@
 import * as SentryCapacitor from '@sentry/capacitor';
 import * as SentryEmber from '@sentry/ember';
-import { InitSentryForEmber } from '@sentry/ember';
-import { BrowserTracing } from '@sentry/tracing';
 import config from 'houseninja/config/environment';
 import isNativePlatform from 'houseninja/utils/is-native-platform';
 
 const sentryOptions = config['@sentry/ember'].sentry;
-const tracingOrigins = config['@sentry/ember'].tracingOrigins;
 
 sentryOptions.environment = config.environment;
 
 export function nativeInit() {
-  SentryCapacitor.init(
-    {
-      ...sentryOptions,
-      integrations: [
-        new BrowserTracing({
-          tracingOrigins,
-        }),
-      ],
-    },
-    SentryEmber.init
-  );
+  SentryCapacitor.init(sentryOptions, SentryEmber.init);
 }
 
 export function webInit() {
-  console.log('Init');
-  InitSentryForEmber({
-    ...sentryOptions,
-    integrations: [
-      new BrowserTracing({
-        tracingOrigins,
-      }),
-    ],
-  });
+  SentryEmber.init(sentryOptions);
 }
 
 export function init() {
