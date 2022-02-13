f57114fd6f76003cd1fb438d5a1000a516a0f03d
Miles Zimmerman <miles@houseninja.co>
2022-02-13 15:24:01 -0800

    add session code comment

--- app/services/session.js
+++ app/services/session.js
@@ -1,6 +1,9 @@
 import BaseSessionService from 'ember-simple-auth/services/session';
 import { service } from '@ember/service';
 
+/**
+ * @see https://github.com/simplabs/ember-simple-auth/blob/master/packages/ember-simple-auth/addon/services/session.js
+ */
 export default class SessionService extends BaseSessionService {
   @service current;
 
