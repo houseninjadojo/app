708e6664e26ab237580ff4c9dee3116c8e9911f2
Miles Zimmerman <miles@houseninja.co>
2022-02-13 17:33:17 -0800

    fix: make it snappy

--- app/routes/dashboard/work-history.js
+++ app/routes/dashboard/work-history.js
@@ -2,21 +2,11 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 
 export default class DashboardWorkHistoryRoute extends Route {
-  @service current;
   @service store;
 
   async model() {
-    return await this.store.query('work-order', {
-      filter: {
-        property: {
-          id: this.current.property.id,
-        },
-      },
-      page: {
-        // offset: 0,
-        number: 1,
-        size: 20,
-      },
+    return await this.store.findAll('work-order', {
+      backgroundReload: true,
     });
   }
 }
