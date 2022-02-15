74eb8974e927e5dffad8374f3fc46870b5f39eff
achillesimperial <achilles@houseninja.co>
2022-02-15 12:30:55 -0500

    Promo code element

---
+++ app/components/house-ninja/referral-code/component.js
@@ -0,0 +1,24 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { Share } from '@capacitor/share';
+
+export default class ServiceAreaComponent extends Component {
+  @service current;
+  @service router;
+  @service store;
+
+  @tracked promoCode =
+    this.current.user.promoCode.isActive && this.current.user.promoCode.code;
+
+  @action
+  async handleClick() {
+    await Share.share({
+      title: '2 months free service with House Ninja', //iOS - Displays in the email subject
+      text: `Use my referral code ${this.promoCode} to get 2 months of free service with House Ninja!`, // iOS - Main content of email and text
+      url: 'https://www.houseninja.co/', // iOS appended to main content (text property)
+      dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
+    });
+  }
+}
