9b296aaa315335a636581e563392f81a2d6c37b0
achillesimperial <achilles@houseninja.co>
2022-02-18 11:35:56 -0500

    Updated share copy

--- app/components/house-ninja/referral-code/component.js
+++ app/components/house-ninja/referral-code/component.js
@@ -14,10 +14,10 @@ export default class ServiceAreaComponent extends Component {
   @action
   async handleClick() {
     await Share.share({
-      title: '2 months free service with House Ninja', //iOS - Displays in the email subject
-      text: `Use my referral code ${this.promoCode} to get 2 months of free service with House Ninja!`, // iOS - Main content of email and text
-      url: 'https://www.houseninja.co/', // iOS appended to main content (text property)
-      dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
+      title: 'Try House Ninja for two months FREE!', //iOS - Displays in the email subject
+      text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${this.promoCode} you’ll get your first two months free!`, // iOS - Main content of email and text
+      url: 'http://www.houseninja.co/join', // iOS appended to main content (text property)
+      // dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
     });
   }
 }
