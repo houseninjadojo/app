9c50b0c21866e955ec2152ed339c68795d85685a
achillesimperial <achilles@houseninja.co>
2022-02-22 17:02:31 -0500

    Intercom condition for web

--- app/app.js
+++ app/app.js
@@ -4,13 +4,16 @@ import loadInitializers from 'ember-load-initializers';
 import config from 'houseninja/config/environment';
 import { init } from 'houseninja/utils/sentry';
 import { Intercom } from '@capacitor-community/intercom';
+import isNativePlatform from 'houseninja/utils/is-native-platform';
 // init sentry
 init();
 
 export default class App extends Application {
   constructor() {
     super(...arguments);
-    Intercom.hideInAppMessages();
+    if (isNativePlatform) {
+      Intercom.hideInAppMessages();
+    }
   }
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
