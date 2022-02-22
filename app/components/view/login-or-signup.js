370acaa1a9bf81ee028d8db9f50240327feb8619
achillesimperial <achilles@houseninja.co>
2022-02-22 15:14:27 -0500

    Using popover for signup modal

--- app/components/view/login-or-signup.js
+++ app/components/view/login-or-signup.js
@@ -1,12 +1,17 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { Browser } from '@capacitor/browser';
+import ENV from 'houseninja/config/environment';
 
 export default class LoginOrSignupViewComponent extends Component {
   @service router;
 
   @action openSignUpViewInSystemBrowser() {
-    window.open('/#/signup', '_system');
+    Browser.open({
+      url: `${ENV.appHost}/signup`,
+      presentationStyle: 'popover',
+    });
   }
 
   @action login() {
