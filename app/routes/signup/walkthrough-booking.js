83a46849825a6de3d2f893a7b262aef73d07ad71
Miles Zimmerman <miles@houseninja.co>
2022-02-23 21:29:36 -0800

    hardcode zip

--- app/routes/signup/walkthrough-booking.js
+++ app/routes/signup/walkthrough-booking.js
@@ -7,6 +7,11 @@ export default class SignupWalkthroughBookingRoute extends Route {
   async model() {
     // @todo
     // we are doing this while we only have 1 service area
-    return await this.store.findAll('service-area').get('firstObject');
+    const serviceAreas = await this.store.query('service-area', {
+      filter: {
+        zipcodes: ['78702'],
+      },
+    });
+    return serviceAreas.get('firstObject');
   }
 }
