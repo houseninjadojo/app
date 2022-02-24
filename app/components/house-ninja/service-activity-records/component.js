30befad683281817320e0abf122f968d04a1129e
achillesimperial <achilles@houseninja.co>
2022-02-24 18:22:07 -0500

    Renamed to ServiceActivityRecords

--- app/components/house-ninja/service-activity-records/component.js
+++ app/components/house-ninja/service-activity-records/component.js
@@ -1,6 +1,6 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
-export default class ServiceActivityComponent extends Component {
+export default class ServiceActivityRecordsComponent extends Component {
   get getActiveTabContent() {
     return this.activeRecords.filter((r) => r.active)[0];
   }
