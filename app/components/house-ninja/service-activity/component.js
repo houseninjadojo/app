8493ad7a6f6740e3c0799b473d3ad3337b8577d5
achillesimperial <achilles@houseninja.co>
2022-02-24 13:45:45 -0500

    Refactored service activity rendering

--- app/components/house-ninja/service-activity/component.js
+++ app/components/house-ninja/service-activity/component.js
@@ -1,12 +1,7 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
 
 export default class ServiceActivityComponent extends Component {
   get getActiveTabContent() {
     return this.activeRecords.filter((r) => r.active)[0];
-  }
-  @action
-  userHasServiceRecords(serviceRecords) {
-    return serviceRecords.length > 0;
   }
 }
