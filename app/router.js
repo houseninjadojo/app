d4c71dd5995912a89a9ee3b306f785cad29a72ee
achillesimperial <achilles@houseninja.co>
2022-02-24 18:23:10 -0500

    Updated router

--- app/router.js
+++ app/router.js
@@ -43,14 +43,13 @@ Router.map(function () {
   this.route('vault', function () {
     this.route('upload');
     this.route('document', function () {
-      this.route('document', { path: '/document/:doc_id' });
-      this.route('edit', { path: '/vault/document/edit/:doc_id' });
+      this.route('index', { path: '/:doc_id' });
+      this.route('edit', { path: '/edit/:doc_id' });
     });
-
     this.route('group', function () {
-      this.route('new', { path: '/group/new' });
-      this.route('group', { path: '/group/:group_id' });
-      this.route('edit', { path: '/vault/group/edit/:group_id' });
+      this.route('index', { path: '/:group_id' });
+      this.route('new');
+      this.route('edit', { path: '/edit/:group_id' });
     });
   });
 });
