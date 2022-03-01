dad475044fa929234a58f4fa2c0aeb668a862b9d
achillesimperial <achilles@houseninja.co>
2022-03-01 16:29:15 -0500

    Progress bar is working

--- app/components/primitive/loader/global.js
+++ app/components/primitive/loader/global.js
@@ -1,14 +1,9 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
-import { action } from '@ember/object';
 import { service } from '@ember/service';
 
 export default class GlobalComponent extends Component {
   @service loader;
-  @tracked isLoading = this.loader.isLoading;
-
-  @action
-  loading() {
-    this.loader = this.loader.isLoading;
+  get loading() {
+    return this.loader.isLoading;
   }
 }
