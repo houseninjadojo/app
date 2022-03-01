93611560b329186f41da62dd8f5b395c921c7500
Miles Zimmerman <miles@houseninja.co>
2022-03-01 12:23:26 -0800

    remove RSVP.hash

--- app/routes/signup/walkthrough-booking.js
+++ app/routes/signup/walkthrough-booking.js
@@ -1,15 +1,14 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
-import RSVP from 'rsvp';
 
 export default class SignupWalkthroughBookingRoute extends Route {
   @service store;
 
   async model() {
-    return RSVP.hash({
+    return {
       property: this.store.peekAll('property').get('firstObject'),
       serviceArea: this.store.peekAll('service-area').get('firstObject'),
       user: this.store.peekAll('user').get('firstObject'),
-    });
+    };
   }
 }
