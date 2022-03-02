c4700bb179a27f9a7e222ea44d14114765903f09
Miles Zimmerman <miles@houseninja.co>
2022-03-02 12:08:25 -0800

    ci(release): use @semantic-release/github for release packaging

--- release.config.js
+++ release.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   dryRun: false,
   debug: true,
-  // branches: [
-  //   'main',
-  //   { name: 'semantic-release', prerelease: true },
-  //   { name: 'testflight', prerelease: true },
-  // ],
   branches: [
     '+([0-9])?(.{+([0-9]),x}).x',
     'main',
     { name: 'semantic-release', prerelease: true },
-    // 'next',
-    // 'next-major',
-    { name: 'beta', prerelease: true },
-    // { name: 'alpha', prerelease: true }
   ],
   plugins: [
     '@semantic-release/commit-analyzer',
@@ -24,18 +15,18 @@ module.exports = {
         changelogFile: 'CHANGELOG.md',
       },
     ],
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     publish: false,
-    //     assets: [
-    //       'CHANGELOG.md',
-    //       'package.json',
-    //       'yarn.lock',
-    //       { path: 'dist', name: 'Ember Application' },
-    //     ]
-    //   },
-    // ],
+    [
+      '@semantic-release/github',
+      {
+        publish: false,
+        assets: [
+          'CHANGELOG.md',
+          'package.json',
+          'yarn.lock',
+          { path: 'dist', name: 'Ember Application' },
+        ]
+      },
+    ],
     // [
     //   '@semantic-release/npm',
     //   {
