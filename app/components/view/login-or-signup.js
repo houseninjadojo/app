c92284e6fbb7b051a3057be5e5df821bff7f8339
achillesimperial <achilles@houseninja.co>
2022-03-02 16:07:09 -0500

    url

--- app/components/view/login-or-signup.js
+++ app/components/view/login-or-signup.js
@@ -2,14 +2,13 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { Browser } from '@capacitor/browser';
-import ENV from 'houseninja/config/environment';
 
 export default class LoginOrSignupViewComponent extends Component {
   @service router;
 
   @action openSignUpViewInSystemBrowser() {
     Browser.open({
-      url: `${ENV.appHost}/signup`,
+      url: `https://app.houseninja.co/signup/`,
       presentationStyle: 'popover',
     });
   }
