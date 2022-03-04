12bbe292f0a518d5c613cecf7132c6c8e22e8ba0
achillesimperial <achilles@houseninja.co>
2022-03-04 10:37:04 -0500

    Handling Share canceled promise rejection

--- app/components/house-ninja/referral-code/component.js
+++ app/components/house-ninja/referral-code/component.js
@@ -3,6 +3,8 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { Share } from '@capacitor/share';
+import { debug } from '@ember/debug';
+import * as Sentry from '@sentry/ember';
 
 export default class ServiceAreaComponent extends Component {
   @service current;
@@ -13,11 +15,16 @@ export default class ServiceAreaComponent extends Component {
 
   @action
   async handleClick() {
-    await Share.share({
-      title: 'Try House Ninja for two months FREE!', //iOS - Displays in the email subject
-      text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${this.promoCode} you’ll get your first two months free!`, // iOS - Main content of email and text
-      url: 'www.houseninja.co/sign-up', // iOS appended to main content (text property)
-      // dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
-    });
+    try {
+      await Share.share({
+        title: 'Try House Ninja for two months FREE!', //iOS - Displays in the email subject
+        text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${this.promoCode} you’ll get your first two months free!`, // iOS - Main content of email and text
+        url: 'www.houseninja.co/sign-up', // iOS appended to main content (text property)
+        // dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
+      });
+    } catch (e) {
+      debug(e);
+      Sentry.captureException(e);
+    }
   }
 }
