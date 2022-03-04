1555f411c969fbcfc9ffb973a0024b56baaac12a
achillesimperial <achilles@houseninja.co>
2022-03-04 17:57:55 -0500

    Checking for native platform

--- app/components/primitive/button/component.js
+++ app/components/primitive/button/component.js
@@ -1,12 +1,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { Haptics, ImpactStyle } from '@capacitor/haptics';
-
+import isNativePlatform from 'houseninja/utils/is-native-platform';
 export default class ButtonComponent extends Component {
   @action
   async handleClick() {
     if (this.args.turnOffHapticFeedback !== true) {
-      await Haptics.impact({ style: ImpactStyle.Light });
+      isNativePlatform() &&
+        (await Haptics.impact({ style: ImpactStyle.Light }));
     }
   }
 }
