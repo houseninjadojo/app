b8a93920c7580cabae2860e99fa41daccf6acbec
achillesimperial <achilles@houseninja.co>
2022-03-07 10:06:38 -0500

    fix: close popover browser post logout

--- app/routes/logout.js
+++ app/routes/logout.js
@@ -1,5 +1,8 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import isNativePlatform from 'houseninja/utils/is-native-platform';
+import { Browser } from '@capacitor/browser';
+import { SplashScreen } from '@capacitor/splash-screen';
 
 export default class LogoutRoute extends Route {
   @service analytics;
@@ -8,11 +11,30 @@ export default class LogoutRoute extends Route {
   @service session;
 
   async beforeModel(transition) {
+    SplashScreen.show({
+      fadeInDuration: 0,
+      fadeOutDuration: 1000,
+      showDuration: 1000,
+      autoHide: false,
+    });
     this.session.requireAuthentication(transition, 'login-or-signup');
     await this.analytics.track('Logout');
     await this.session.invalidate();
     await this.analytics.reset();
     await this.intercom.logout();
+    await this.closeBrowser();
     this.router.transitionTo('index');
+  }
+
+  async closeBrowser() {
+    if (isNativePlatform()) {
+      try {
+        Browser.removeAllListeners();
+        await Browser.close();
+        return SplashScreen.hide();
+      } catch {
+        return SplashScreen.hide();
+      }
+    }
   }
 }
