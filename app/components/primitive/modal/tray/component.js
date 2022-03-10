3b009fc8a47c4aa34e3f05219cfee4b5d9e0c4b1
achillesimperial <achilles@houseninja.co>
2022-03-10 11:57:35 -0500

    action sheet on back when settings changed detected

--- app/components/primitive/modal/tray/component.js
+++ app/components/primitive/modal/tray/component.js
@@ -1,21 +1,63 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
 import { action } from '@ember/object';
+import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 
 export default class TrayComponent extends Component {
   @service router;
 
+  options = [
+    {
+      title: 'Save changes',
+    },
+    {
+      title: 'Cancel',
+      style: ActionSheetButtonStyle.Cancel,
+    },
+    {
+      title: 'Discard changes',
+      style: ActionSheetButtonStyle.Destructive,
+    },
+  ];
+
   @action
-  goBack() {
-    this.router.transitionTo(this.args.backRoute);
-    if (this.args.onBack) {
-      this.args.onBack();
+  async goBack() {
+    if (!this.args.formIsInvalid) {
+      let result = await this.confirm();
+      let choice = this.options[result.index].title;
+
+      switch (choice) {
+        case this.options[0].title:
+          this.callSaveAction();
+          this.router.transitionTo(this.args.backRoute);
+          this.args.onBack && this.args.onBack();
+          break;
+        case this.options[1].title:
+          break;
+        case this.options[2].title:
+          this.router.transitionTo(this.args.backRoute);
+          this.args.onBack && this.args.onBack();
+          break;
+      }
+    } else {
+      this.router.transitionTo(this.args.backRoute);
+      this.args.onBack && this.args.onBack();
     }
   }
 
   @action
+  async confirm() {
+    const result = await ActionSheet.showActions({
+      title: 'Unsaved changes',
+      message: 'What would you like to do?',
+      options: this.options,
+    });
+    return result;
+  }
+
+  @action
   async callSaveAction() {
-    if (this.args.saveAction) {
+    if (!this.args.formIsInvalid && this.args.saveAction) {
       await this.args.saveAction();
     }
   }
