078747cda8bc05ff2cd93f7b0c30c9577156444b
Achilles Imperial <achilles@houseninja.co>
2022-03-14 09:35:05 -0400

    chore: portal design adjustments (#441)
    * Common Request Item Style
    
    * Preventative Maintenance Image Overlay Text Size
    
    * Home Care Tip Style

--- mirage/fixtures/common-requests.js
+++ mirage/fixtures/common-requests.js
@@ -2,62 +2,62 @@ const baseURI = '/assets/images/common-requests';
 
 export default [
   {
-    caption: 'Request Plumbing Service',
+    caption: 'Plumbing Service',
     imgURI: `${baseURI}/marian-florinel-condruz-C-oYJoIfgCs-unsplash.jpg`,
     defaultHnChatMessage: 'Hello, I would like to request plumbing service.',
   },
   {
-    caption: 'Request A/C Repair Service',
+    caption: 'A/C Repair Service',
     imgURI: `${baseURI}/raychan-KGqif07slUY-unsplash.jpg`,
     defaultHnChatMessage: 'Hello, I would like to request A/C repair service.',
   },
   {
-    caption: 'Request Appliance Repair',
+    caption: 'Appliance Repair',
     imgURI: `${baseURI}/kam-idris-Ot2iTXgC6fY-unsplash.jpg`,
     defaultHnChatMessage:
       'Hello, I would like to request appliance repair service.',
   },
   {
-    caption: 'Schedule Outdoor Cleaning',
+    caption: 'Outdoor Cleaning',
     imgURI: `${baseURI}/daniel-watson-8vBpYpTGo90-unsplash.jpg`,
     defaultHnChatMessage:
       'Hello, I would like to schedule an outdoor cleaning.',
   },
   {
-    caption: 'Request Lighting Upgrade',
+    caption: 'Lighting Upgrade',
     imgURI: `${baseURI}/etienne-girardet-NGb91VwnOWY-unsplash.jpg`,
     defaultHnChatMessage:
       'Hello, I would like to explore upgrading my lighting. Can you help me?',
   },
   {
-    caption: 'Request Custom Storage Solution',
+    caption: 'Custom Storage Solution',
     imgURI: `${baseURI}/pickawood-LnXm8xLCFtg-unsplash.jpg`,
     defaultHnChatMessage:
       'Hello, I would like to explore a custom storage solution. Can you assist?',
   },
   {
-    caption: 'Request Electrical Outlet Fix',
+    caption: 'Electrical Outlet Fix',
     imgURI: `${baseURI}/clint-patterson-exfrR9KkzlE-unsplash.jpg`,
     defaultHnChatMessage: 'Hello, I would like help with an electrical outlet.',
   },
   {
-    caption: 'Request Painting Work',
+    caption: 'Painting Work',
     imgURI: `${baseURI}/theme-photos-Cl-OpYWFFm0-unsplash.jpg`,
     defaultHnChatMessage: 'Hello, I am interested in a painting service.',
   },
   {
-    caption: 'Request Carpet/Upholstery Cleaning',
+    caption: 'Carpet/Upholstery Cleaning',
     imgURI: `${baseURI}/the-creative-exchange-uSFYHjmqPAE-unsplash.jpg`,
     defaultHnChatMessage:
       'Hello, I would like to have my carpet or upholstery cleaned.',
   },
   {
-    caption: 'Request BBQ/Grill Service',
+    caption: 'BBQ/Grill Service',
     imgURI: `${baseURI}/adam-mills-SHFQI_DGgAU-unsplash.jpg`,
     defaultHnChatMessage: 'Hello, my BBQ is in need of service.',
   },
   {
-    caption: 'Request Chimney Cleaning',
+    caption: 'Chimney Cleaning',
     imgURI: `${baseURI}/vlado-paunovic-u1Q3Aem_TWc-unsplash.jpg`,
     defaultHnChatMessage:
       "Hello, I'd like to have my chimney cleaned. Can you help?",
