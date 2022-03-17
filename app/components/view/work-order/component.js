5817fabf4e588c1cf0cd396a2f92695f728694dd
achillesimperial <achilles@houseninja.co>
2022-03-17 19:30:04 -0400

    Initial WorkOrder view templating

---
+++ app/components/view/work-order/component.js
@@ -0,0 +1,47 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { service } from '@ember/service';
+import { Intercom } from '@capacitor-community/intercom';
+import { workOrderStatus } from 'houseninja/data/work-order-status';
+import { toCamel } from 'houseninja/utils/string-helpers';
+
+export default class WorkOrderViewComponent extends Component {
+  @service router;
+  @service intercom;
+
+  cancelMessage = 'I would like to cancel this work order.';
+  issueMessage = 'I have an issue with my work order.';
+
+  content = {
+    approvePayment: 'ApprovePayment',
+    paymentFailed: 'FailedPayment',
+    closed: 'Closed',
+    default: 'Default',
+  };
+
+  get contentType() {
+    const status = workOrderStatus[toCamel(this.args.model.status)];
+    switch (status) {
+      case workOrderStatus.invoiceSentToCustomer:
+        return this.content.approvePayment;
+      case workOrderStatus.paymentFailed:
+        return this.content.paymentFailed;
+      case workOrderStatus.closed:
+        return this.content.closed;
+      default:
+        return this.content.default;
+    }
+  }
+
+  @action
+  selectRoute(route) {
+    if (typeof route === 'object') {
+      this.router.transitionTo(`work-order`, route.id);
+    }
+  }
+
+  @action
+  async displayMessageComposer(message) {
+    await Intercom.displayMessageComposer({ message });
+  }
+}
