2790e3683d65548a60a79a28c768cae748c41ec1
Achilles Imperial <achilles@houseninja.co>
2022-03-17 17:59:19 -0400

    fix: portal updates (#446)
    * Hide action button behavior for Modal::Tray
    
    * Removing contact us template and route
    
    * Flagging button disabled bug
    
    * Demphasizing Home Care Tip button

--- app/components/primitive/button/component.js
+++ app/components/primitive/button/component.js
@@ -4,6 +4,10 @@ import { service } from '@ember/service';
 export default class ButtonComponent extends Component {
   @service haptics;
 
+  get isDisabled() {
+    return this.args.isDisabled;
+  }
+
   @action
   async handleClick() {
     this.haptics.giveFeedback(this.args.turnOffHapticFeedback);
