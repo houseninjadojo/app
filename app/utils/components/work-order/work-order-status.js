951ff24246def268a92e6ff383fc3e2e403b2af2
achillesimperial <achilles@houseninja.co>
2022-03-23 10:16:34 -0400

    Adding additional work order status label cases

--- app/utils/components/work-order/work-order-status.js
+++ app/utils/components/work-order/work-order-status.js
@@ -6,7 +6,14 @@ import {
 export const getWorkOrderTag = (status) => {
   switch (status) {
     case workOrderStatus.invoicePaidByCustomer:
+      // For when the review feature is developed.
+      // return {
+      //   label: getWorkOrderStatusLabel(status),
+      //   type: 'accent',
+      // };
+      return;
     case workOrderStatus.closed:
+    case workOrderStatus.cancelled:
       return;
     case workOrderStatus.invoiceSentToCustomer:
       return {
@@ -18,8 +25,17 @@ export const getWorkOrderTag = (status) => {
         label: getWorkOrderStatusLabel(status),
         type: 'alert',
       };
-    case workOrderStatus.workOrderInititated:
     case workOrderStatus.workScheduled:
+      return {
+        label: getWorkOrderStatusLabel(status),
+        type: 'primary',
+      };
+    case workOrderStatus.paused:
+      return {
+        label: getWorkOrderStatusLabel(status),
+        type: 'disabled',
+      };
+    case workOrderStatus.workOrderInititated:
     default:
       return {
         label: getWorkOrderStatusLabel(status),
