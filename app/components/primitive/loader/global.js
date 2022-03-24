88a491a33c51b5335670a74ede65097cda18efa3
achillesimperial <achilles@houseninja.co>
2022-03-24 17:48:48 -0400

    moving global loader

--- app/components/primitive/loader/global.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { service } from '@ember/service';
-
-export default class GlobalComponent extends Component {
-  @service loader;
-  get loading() {
-    return this.loader.isLoading;
-  }
-}
