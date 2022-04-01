d6c835472e487f677624eabb167a3c0c65297f10
achillesimperial <achilles@houseninja.co>
2022-04-01 11:35:59 -0400

    Delete this document confirmation

--- app/components/view/vault/document/upsert.js
+++ app/components/view/vault/document/upsert.js
@@ -1,7 +1,9 @@
 import Component from '@glimmer/component';
 import { service } from '@ember/service';
-// import { action } from '@ember/object';
+import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { inputValidation } from 'houseninja/utils/components/input-validation';
+import { ActionSheet, ActionSheetButtonStyle } from '@capacitor/action-sheet';
 
 // import { Camera, CameraResultType } from '@capacitor/camera';
 
@@ -11,6 +13,7 @@ export default class VaultDocumentUpsertComponent extends Component {
   @service router;
   @service view;
 
+  @tracked formIsInvalid = true;
   @tracked media = this.camera.image;
   @tracked mediaUrl =
     this.args.model.document.url || (this.media && this.media.webPath);
@@ -58,4 +61,48 @@ export default class VaultDocumentUpsertComponent extends Component {
       value: this.args.model.document && this.args.model.document.groupId,
     },
   ];
+
+  options = [
+    {
+      title: 'Keep this document',
+      style: ActionSheetButtonStyle.Cancel,
+    },
+    {
+      title: 'Delete this document',
+      style: ActionSheetButtonStyle.Destructive,
+    },
+  ];
+
+  @action
+  delete() {
+    const response = this.confirm();
+  }
+
+  @action
+  async confirm() {
+    const result = await ActionSheet.showActions({
+      title: 'You are about to delete this document.',
+      message: 'What would you like to do?',
+      options: this.options,
+    });
+    return result;
+  }
+
+  @action
+  resetForm() {
+    this.documentInfo.name = this.args.model.document.name || this.mediaUrl;
+    this.documentInfo.description =
+      this.args.model.document.description || null;
+    this.documentInfo.documentGroup = this.args.model.document.groupId || null;
+    this.formIsInvalid = true;
+  }
+
+  @action
+  validateForm(e) {
+    this.documentInfo[e.target.id] = e.target.value;
+    this.fields.filter((f) => f.id === e.target.id)[0].value =
+      this.documentInfo[e.target.id];
+
+    this.formIsInvalid = inputValidation(this.fields, []).isInvalid;
+  }
 }
