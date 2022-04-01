801cf8dc9e138642fb1cbd84d188c9a8d9d3ed5b
achillesimperial <achilles@houseninja.co>
2022-03-31 20:47:28 -0400

    Updates to route model hooks, replaced data-stub

--- app/routes/vault/group/index.js
+++ app/routes/vault/group/index.js
@@ -1,23 +1,34 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { getIconUri } from 'houseninja/utils/components/formatting';
-import { vault } from 'houseninja/data/document-stub';
 
 export default class VaultGroupIndexRoute extends Route {
   @service router;
+  @service store;
 
-  documents = vault.documentStub.map((d) => {
-    return {
-      ...d,
-      iconUri: getIconUri(d.type),
-    };
-  });
+  documents = [];
 
-  model(params) {
-    const model = vault.groupStub.filter((g) => g.id === params.group_id)[0];
-    model['documents'] = this.documents.filter((d) => {
-      return d.groupId === params.group_id;
-    });
+  async model({ group_id }) {
+    const documents = await this.store.findAll('document');
+
+    this.documents = documents
+      .filter((d) => d.groupId === group_id)
+      .map((d) => {
+        const { id, type, name, description, url, groupId } = d;
+        return {
+          id,
+          type,
+          name,
+          description,
+          url,
+          groupId,
+          iconUri: getIconUri(d.type),
+          ...d,
+        };
+      });
+
+    const model = await this.store.findRecord('document-group', group_id);
+    model['documents'] = this.documents;
 
     return model;
   }
