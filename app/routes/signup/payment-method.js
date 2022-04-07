dd4c607030a98e2a1161bdc2ca9c73d16338a566
Miles Zimmerman <miles@houseninja.co>
2022-04-07 13:34:23 -0700

    generate subscription in payment-method component

--- app/routes/signup/payment-method.js
+++ app/routes/signup/payment-method.js
@@ -3,15 +3,13 @@ import { service } from '@ember/service';
 import { captureException } from 'houseninja/utils/sentry';
 import { task } from 'ember-concurrency';
 import { PAYMENT_METHOD } from 'houseninja/data/enums/onboarding-step';
-import { isEmpty } from '@ember/utils';
 
 export default class SignupPaymentMethodRoute extends Route {
   @service store;
   @service onboarding;
 
   async model() {
-    this.generateSubscription.perform();
-    // this.rehydrateOrGenerateSubscription.perform();
+    this.fetchSubscriptionPlan.perform();
     return this.onboarding.fetchLocalModel('credit-card');
   }
 
@@ -19,28 +17,14 @@ export default class SignupPaymentMethodRoute extends Route {
     this.onboarding.completeStep(PAYMENT_METHOD);
   }
 
-  @task *generateSubscription() {
-    const subscriptionPlan = yield this.fetchSubscriptionPlan.perform();
-    this.store.createRecord('subscription', {
-      subscriptionPlan,
-    });
-  }
-
-  @task({ drop: true }) *rehydrateOrGenerateSubscription() {
-    if (isEmpty(this.store.peekFirst('subscription')?.id)) {
-      yield this.generateSubscription.perform();
-    }
-  }
-
   @task({ drop: true }) *fetchSubscriptionPlan() {
-    let subscriptionPlan = this.onboarding.fetchLocalModel('subscription-plan');
-    if (isEmpty(subscriptionPlan)) {
-      try {
-        subscriptionPlan = yield this.store.findFirst('subscription-plan');
-      } catch (e) {
-        captureException(e);
-      }
+    // while we are only using the monthly plan, we can
+    // simply reload the subscription plan instance from the API
+    this.store.unloadAll('subscription-plan');
+    try {
+      return yield this.store.findFirst('subscription-plan');
+    } catch (e) {
+      captureException(e);
     }
-    return subscriptionPlan;
   }
 }
