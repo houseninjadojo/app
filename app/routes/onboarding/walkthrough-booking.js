fb13d34d270c1b4d949bfab3eb29f897f84f39bf
achillesimperial <achilles@houseninja.co>
2022-05-03 10:40:26 -0400

    route, model, template

--- app/routes/onboarding/walkthrough-booking.js
+++ app/routes/onboarding/walkthrough-booking.js
@@ -2,15 +2,27 @@ import Route from '@ember/routing/route';
 import { service } from '@ember/service';
 import { WALKTHROUGH_BOOKING } from 'houseninja/data/enums/onboarding-step';
 import RSVP from 'rsvp';
+import { isBlank } from '@ember/utils';
 
 export default class OnboardingWalkthroughBookingRoute extends Route {
+  @service current;
   @service onboarding;
+  @service store;
 
-  model() {
+  async model() {
+    if (isBlank(this.current.property)) {
+      await this.current.loadUser();
+    }
+    const serviceArea = await this.store.queryRecord('service-area', {
+      filter: {
+        zipcodes: [this.current.property.zipcode],
+      },
+    });
+
     return RSVP.hash({
-      property: this.onboarding.localModel('property'),
-      serviceArea: this.onboarding.localModel('service-area'),
-      user: this.onboarding.localModel('user'),
+      user: this.current.user,
+      property: this.current.property,
+      serviceArea,
     });
   }
 
