f9814935e5af33925bba2703fd1174df5460f453
Miles Zimmerman <miles@houseninja.co>
2022-05-10 20:10:07 -0700

    fix lint

--- tests/unit/models/line-item-test.js
+++ tests/unit/models/line-item-test.js
@@ -1,11 +1,11 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'houseninja/tests/helpers';
 
-module('Unit | Model | line item', function(hooks) {
+module('Unit | Model | line item', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('line-item', {});
     assert.ok(model);
