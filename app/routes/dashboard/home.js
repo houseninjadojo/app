dd44d49c4c4783f6ea24c8400ac43d7e87c7486a
achillesimperial <achilles@houseninja.co>
2022-05-13 16:21:11 -0400

    dashboard.home route model cleanup

---
+++ app/routes/dashboard/home.js
@@ -0,0 +1,44 @@
+import Route from '@ember/routing/route';
+import { service } from '@ember/service';
+import { isBlank } from '@ember/utils';
+import RSVP from 'rsvp';
+import { NATIVE_MOBILE_ROUTE } from 'houseninja/data/enums/routes';
+
+export default class DashboarHomeRoute extends Route {
+  @service current;
+  @service session;
+  @service store;
+
+  beforeModel(transition) {
+    this.session.requireAuthentication(
+      transition,
+      NATIVE_MOBILE_ROUTE.AUTH.LOGIN_OR_SIGNUP
+    );
+  }
+
+  async model() {
+    const { user_id } = this.session.data.authenticated.userinfo;
+    let user = null;
+    let property = null;
+
+    if (isBlank(this.current.user)) {
+      user = await this.store.findRecord('user', user_id, {
+        include: 'properties',
+      });
+      property = user.get('properties.firstObject');
+    } else {
+      property = this.current.user.get('properties.firstObject');
+    }
+
+    return RSVP.hash({
+      user: this.current.user || user,
+      property: this.current.property || property,
+      commonRequests: this.store.findAll('common-request', {
+        backgroundReload: true,
+      }),
+      homeCareTips: this.store.findAll('home-care-tip', {
+        backgroundReload: true,
+      }),
+    });
+  }
+}
