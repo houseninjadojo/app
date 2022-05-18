e146af06c04d85cbfefc9a7c181c090b4428cf2c
Miles Zimmerman <miles@houseninja.co>
2022-05-17 20:45:21 -0700

    adding for branch live testing

--- app/routes/p/approve-payment.js
+++ app/routes/p/approve-payment.js
@@ -6,28 +6,32 @@ export default class ApprovePaymentRoute extends Route {
   @service router;
   @service store;
 
-  async model({ access_token }) {
-    const invoice = await this.store.findRecord('invoice', access_token, {
-      include: [
-        'payment',
-        'work_order',
-        'work_order.property',
-        'work_order.property.user',
-        'work_order.property.user.payment_methods',
-      ].join(','),
-    });
+  model({ access_token }) {
+    return this.store
+      .findRecord('invoice', access_token, {
+        include: [
+          // 'payment',
+          'work_order',
+          'work_order.property',
+          'work_order.property.user',
+          'work_order.property.user.payment_methods',
+        ].join(','),
+      })
+      .then((invoice) => {
+        return invoice.get('work-order');
+      });
 
-    return {
-      invoice,
-      workOrder: invoice.get('workOrder'),
-      property: invoice.get('workOrder.property'),
-      user: invoice.get('workOrder.property.user'),
-      paymentMethod: invoice.get(
-        'workOrder.property.user.paymentMethods.firstObject'
-      ),
-      statusLabel: invoice.get('workOrder.statusLabel'),
-      vendor: invoice.get('workOrder.vendor'),
-      formattedTotal: invoice.formattedTotal,
-    };
+    // return {
+    //   invoice,
+    //   workOrder: invoice.get('workOrder'),
+    //   property: invoice.get('workOrder.property'),
+    //   user: invoice.get('workOrder.property.user'),
+    //   paymentMethod: invoice.get(
+    //     'workOrder.property.user.paymentMethods.firstObject'
+    //   ),
+    //   statusLabel: invoice.get('workOrder.statusLabel'),
+    //   vendor: invoice.get('workOrder.vendor'),
+    //   formattedTotal: invoice.formattedTotal,
+    // };
   }
 }
