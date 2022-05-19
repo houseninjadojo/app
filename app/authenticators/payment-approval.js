3e4b0928ef1e5228525d20075d8f42e5fad7fee8
Miles Zimmerman <miles@houseninja.co>
2022-05-19 13:26:18 -0700

    convert payment approval authenticator functions to promise

--- app/authenticators/payment-approval.js
+++ app/authenticators/payment-approval.js
@@ -3,11 +3,11 @@ import BaseAuthenticator from 'ember-simple-auth/authenticators/base';
 export default class PaymentApprovalAuthenticator extends BaseAuthenticator {
   access_token = null;
 
-  restore(data) {
+  async restore(data) {
     return data;
   }
 
-  authenticate(access_token) {
+  async authenticate(access_token) {
     this.access_token = access_token;
     return {
       access_token,
@@ -15,7 +15,7 @@ export default class PaymentApprovalAuthenticator extends BaseAuthenticator {
     };
   }
 
-  invalidate() {
+  async invalidate() {
     this.access_token = null;
     return;
   }
