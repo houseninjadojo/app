82e4f1950deb5ff8dbcb0664b6dd03a59836121d
achillesimperial <achilles@houseninja.co>
2022-05-25 10:40:30 -0400

    ensuring current.user.id is available to settings.contact user lookup

--- app/routes/settings/contact.js
+++ app/routes/settings/contact.js
@@ -1,14 +1,21 @@
 import Route from '@ember/routing/route';
 import { service } from '@ember/service';
+import { isBlank } from '@ember/utils';
 
 export default class SettingsContactRoute extends Route {
   @service current;
   @service store;
 
   async model() {
-    const userId = this.current.user.id;
-    return await this.store.findRecord('user', userId, {
-      backgroundReload: true,
-    });
+    let userId = null;
+    let model = null;
+    if (isBlank(this.current.user)) {
+      await this.current.loadUser();
+      userId = this.current.user.id;
+      model = await this.store.findRecord('user', userId, {
+        backgroundReload: true,
+      });
+    }
+    return model;
   }
 }
