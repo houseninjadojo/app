c1c2ce1b62961e485f337611563b2949d6ac7f55
achillesimperial <achilles@houseninja.co>
2022-06-07 12:41:29 -0400

    update work order mirage factory

--- mirage/factories/disabled/work-order.js
+++ mirage/factories/disabled/work-order.js
@@ -1,14 +1,24 @@
-import { Factory } from 'miragejs';
+import { Factory, association } from 'miragejs';
 import faker from '@faker-js/faker';
 import moment from 'moment';
+import { workOrderStatus } from 'houseninja/data/work-order-status';
 
-function pastOrFuture() {
+const pastOrFuture = () => {
   return faker.random.boolean()
     ? faker.date.past(1, moment())
     : faker.date.future(1, moment());
-}
+};
+
+const getRandomProperty = (obj) => {
+  const keys = Object.keys(obj);
+  const randomKey = keys[Math.floor(Math.random() * keys.length)];
+
+  return obj[randomKey];
+};
 
 export default Factory.extend({
+  invoice: association(),
+
   scheduledDate() {
     return moment(pastOrFuture()).format('MM/DD/YY');
   },
@@ -26,10 +36,15 @@ export default Factory.extend({
   },
 
   status() {
-    return 'open';
+    return getRandomProperty(workOrderStatus);
+    // return 'closed';
   },
 
   invoiceUri() {
     return '';
+  },
+
+  notes() {
+    return faker.lorem.sentence(7);
   },
 });
