67c13dec941cb0e6c69add8f64efe85b500d0032
achillesimperial <achilles@houseninja.co>
2022-06-27 09:09:39 -0700

    update session service with terminate method

--- app/services/session.js
+++ app/services/session.js
@@ -1,11 +1,19 @@
 import BaseSessionService from 'ember-simple-auth/services/session';
 import { service } from '@ember/service';
-
+import { Browser } from '@capacitor/browser';
+import { SplashScreen } from '@capacitor/splash-screen';
+import Sentry from 'houseninja/utils/sentry';
+import isNativePlatform from 'houseninja/utils/is-native-platform';
+import { NATIVE_MOBILE_ROUTE } from 'houseninja/data/enums/routes';
 /**
  * @see https://github.com/simplabs/ember-simple-auth/blob/master/packages/ember-simple-auth/addon/services/session.js
  */
 export default class SessionService extends BaseSessionService {
   @service current;
+  @service analytics;
+  @service intercom;
+  @service router;
+  @service store;
 
   async handleAuthentication() {
     super.handleAuthentication(...arguments);
@@ -29,6 +37,44 @@ export default class SessionService extends BaseSessionService {
         this.current.loadIdentifyAndTrack.perform();
       } catch (err) {
         await this.invalidate();
+      }
+    }
+  }
+
+  async terminate(transition = null) {
+    if (isNativePlatform()) {
+      SplashScreen.show({
+        fadeInDuration: 0,
+        fadeOutDuration: 1000,
+        showDuration: 1000,
+        autoHide: false,
+      });
+    }
+
+    if (transition) {
+      this.requireAuthentication(
+        transition,
+        NATIVE_MOBILE_ROUTE.AUTH.LOGIN_OR_SIGNUP
+      );
+    }
+
+    await this.analytics.track('Logout');
+    await this.invalidate();
+    await this.analytics.reset();
+    await this.intercom.logout();
+    await this._closeBrowser();
+    this.router.transitionTo('index');
+  }
+
+  async _closeBrowser() {
+    if (isNativePlatform()) {
+      try {
+        Browser.removeAllListeners();
+        await Browser.close();
+      } catch (e) {
+        Sentry.captureException(e);
+      } finally {
+        await SplashScreen.hide();
       }
     }
   }
