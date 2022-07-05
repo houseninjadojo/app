f7d803afc0018390a237488189d22e65aaba12f9
achillesimperial <achilles@houseninja.co>
2022-07-05 15:11:16 -0700

    PAYMENTS PT1

--- app/components/view/work-order/content/default.js
+++
@@ -1,8 +0,0 @@
-import Component from '@glimmer/component';
-import { isCompletedWorkOrder } from 'houseninja/utils/components/work-order/work-order-status';
-
-export default class WorkOrderDefaultViewContentComponent extends Component {
-  get hideCancelRequestButton() {
-    return isCompletedWorkOrder(this.args.model.status);
-  }
-}
