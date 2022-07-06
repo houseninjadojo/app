56fe8816f6526714860f6939dc07ae1030f493b7
Tony Miller <mcfiredrill@gmail.com>
2022-07-06 18:52:17 +0900

    Revert "make everything a getter in handle-it/component"
    This reverts commit 9eb6704d1da4863486c3c5aa414b99adaaa359d9.

--- app/components/view/handle-it/component.js
+++ app/components/view/handle-it/component.js
@@ -23,76 +23,64 @@ export default class HandleItComponent extends Component {
     },
   ];
 
-  get activeWorkOrders() {
-    return this.args.workOrders
-      ?.filter((w) => {
-        return isActiveWorkOrder(w.status);
-      })
-      ?.map((w) => {
-        return {
-          id: w.id,
-          name: w.description,
-          description:
-            w.status !== workOrderStatus.paused && w.scheduledDate
-              ? w.vendor
-              : null,
-          scheduledDate:
-            w.status !== workOrderStatus.paused ? w.scheduledDate : null,
-          scheduledTime:
-            w.status !== workOrderStatus.paused && w.scheduledDate
-              ? w.scheduledTime
-              : null,
-          status: w.status,
-          tag: w.status && getWorkOrderTag(w.status),
-          ...w,
-        };
-      });
-  }
-
-  get failedPaymentWorkOrders() {
-    return filterWorkOrdersFor(
-      WORK_ORDER_FILTER.FAILED_PAYMENT,
-      this.activeWorkOrders
-    )?.sort((a, b) => {
-      return newestToOldest(a, b);
+  activeWorkOrders = this.args.workOrders
+    ?.filter((w) => {
+      return isActiveWorkOrder(w.status);
+    })
+    ?.map((w) => {
+      return {
+        id: w.id,
+        name: w.description,
+        description:
+          w.status !== workOrderStatus.paused && w.scheduledDate
+            ? w.vendor
+            : null,
+        scheduledDate:
+          w.status !== workOrderStatus.paused ? w.scheduledDate : null,
+        scheduledTime:
+          w.status !== workOrderStatus.paused && w.scheduledDate
+            ? w.scheduledTime
+            : null,
+        status: w.status,
+        tag: w.status && getWorkOrderTag(w.status),
+        ...w,
+      };
     });
-  }
 
-  get approvePaymentWorkOrders() {
-    return filterWorkOrdersFor(
-      WORK_ORDER_FILTER.APPROVE_PAYMENT,
-      this.activeWorkOrders
-    )?.sort((a, b) => {
-      return newestToOldest(a, b);
-    });
-  }
+  failedPaymentWorkOrders = filterWorkOrdersFor(
+    WORK_ORDER_FILTER.FAILED_PAYMENT,
+    this.activeWorkOrders
+  )?.sort((a, b) => {
+    return newestToOldest(a, b);
+  });
 
-  get remainingBookedWorkOrders() {
-    return filterWorkOrdersFor(
-      WORK_ORDER_FILTER.BOOKED_NOT_APPROVE_AND_NOT_FAILED,
-      this.activeWorkOrders
-    )?.sort((a, b) => {
-      return newestToOldest(a, b);
-    });
-  }
+  approvePaymentWorkOrders = filterWorkOrdersFor(
+    WORK_ORDER_FILTER.APPROVE_PAYMENT,
+    this.activeWorkOrders
+  )?.sort((a, b) => {
+    return newestToOldest(a, b);
+  });
 
-  get nonBookedWorkOrders() {
-    return filterWorkOrdersFor(
-      WORK_ORDER_FILTER.NOT_BOOKED_NOT_APPROVE_AND_NOT_FAILED,
-      this.activeWorkOrders
-    )?.sort((a, b) => {
-      return newestToOldest(a, b);
-    });
-  }
+  remainingBookedWorkOrders = filterWorkOrdersFor(
+    WORK_ORDER_FILTER.BOOKED_NOT_APPROVE_AND_NOT_FAILED,
+    this.activeWorkOrders
+  )?.sort((a, b) => {
+    return newestToOldest(a, b);
+  });
 
-  get pausedWorkOrders() {
-    return filterWorkOrdersFor(
-      WORK_ORDER_FILTER.PAUSED,
-      this.activeWorkOrders
-    )?.sort((a, b) => {
-      return newestToOldest(a, b);
-    });
-  }
+  nonBookedWorkOrders = filterWorkOrdersFor(
+    WORK_ORDER_FILTER.NOT_BOOKED_NOT_APPROVE_AND_NOT_FAILED,
+    this.activeWorkOrders
+  )?.sort((a, b) => {
+    return newestToOldest(a, b);
+  });
+
+  pausedWorkOrders = filterWorkOrdersFor(
+    WORK_ORDER_FILTER.PAUSED,
+    this.activeWorkOrders
+  )?.sort((a, b) => {
+    return newestToOldest(a, b);
+  });
 
   get displayedWorkOrders() {
     return [
