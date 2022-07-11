53ba28dcd3c227575c9727540178b39732d0bcfc
Tony Miller <mcfiredrill@gmail.com>
2022-07-11 12:57:01 +0900

    feat: expire local storage items after some time
    I set the default to 2 hours, we can tweak this however is needed.

--- app/services/storage.js
+++ app/services/storage.js
@@ -16,11 +16,18 @@ export default class StorageService extends Service {
   }
 
   async getLocal(key) {
-    return await getLocalStorage(key);
+    let item = await getLocalStorage(key);
+    if (item.expiresAt < new Date()) {
+      return item.value;
+    }
   }
 
-  async setLocal(key, value) {
-    await setLocalStorage(key, value);
+  async setLocal(key, value, ttlHours = 2) {
+    let time = new Date();
+    await setLocalStorage(key, {
+      value: value,
+      expiresAt: time.setHours(time.getHours() + ttlHours),
+    });
   }
 
   async clearLocal() {
