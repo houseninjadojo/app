6292673b1203234da28a62ca4b77c02c63292302
Miles Zimmerman <miles@houseninja.co>
2022-07-14 07:24:12 -0700

    fix mirage

--- mirage/factories/invoice.js
+++ mirage/factories/invoice.js
@@ -3,7 +3,7 @@ import { faker } from '@faker-js/faker';
 
 export default Factory.extend({
   document: association(),
-  receipt: association('document'),
+  receipt: association(),
 
   description() {
     return 'Invoice #' + faker.datatype.number({ min: 10000, max: 99999 });
