08dedcae3599d8d27ee7071e5e8fac7cd9dd616c
Tony Miller <mcfiredrill@gmail.com>
2022-07-15 14:46:59 +0900

    change ttl to minutes

--- app/services/storage.js
+++ app/services/storage.js
@@ -17,16 +17,20 @@ export default class StorageService extends Service {
 
   async getLocal(key) {
     let item = await getLocalStorage(key);
-    if (item && item.expiresAt < new Date()) {
-      return item.value;
+    if (item && item.expiresAt) {
+      if (item.expiresAt < new Date()) {
+        return item.value;
+      }
+    } else {
+      return item;
     }
   }
 
-  async setLocal(key, value, ttlHours = 2) {
+  async setLocal(key, value, ttlMinutes = 2) {
     let time = new Date();
     await setLocalStorage(key, {
       value: value,
-      expiresAt: time.setHours(time.getHours() + ttlHours),
+      expiresAt: time.setHours(time.getMinutes() + ttlMinutes),
     });
   }
 
