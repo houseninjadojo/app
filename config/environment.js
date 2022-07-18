39be4a2f1a519c334af54b9f113defe02adf1aa5
Miles Zimmerman <miles@houseninja.co>
2022-07-18 03:28:31 -0700

    remove date extend protoype

--- config/environment.js
+++ config/environment.js
@@ -14,8 +14,7 @@ module.exports = function (environment) {
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
       EXTEND_PROTOTYPES: {
-        // Prevent Ember Data from overriding Date.parse.
-        Date: false,
+        // Array: false,
       },
     },
 
