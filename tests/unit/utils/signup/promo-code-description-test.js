0f15c8e49e0735cd1aa298261afc317d15f57da7
Miles Zimmerman <miles@houseninja.co>
2022-07-19 22:13:37 -0700

    broken spec

--- tests/unit/utils/signup/promo-code-description-test.js
+++ tests/unit/utils/signup/promo-code-description-test.js
@@ -1,10 +1,15 @@
-import signupPromoCodeDescription from 'houseninja/utils/signup/promo-code-description';
+import {
+  signupPromoCodeDescription,
+  signupPromoCodeAlert,
+} from 'houseninja/utils/signup/promo-code-description';
 import { module, test } from 'qunit';
 
 module('Unit | Utility | signup/promo-code-description', function () {
   // TODO: Replace this with your real tests.
   test('it works', function (assert) {
     let result = signupPromoCodeDescription();
+    let result2 = signupPromoCodeAlert();
     assert.ok(result);
+    assert.ok(result2);
   });
 });
