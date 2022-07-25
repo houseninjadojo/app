3771e9edda84c1e2523b7e88b5e6a5570268f996
Tony Miller <mcfiredrill@gmail.com>
2022-07-25 16:21:42 +0900

    fix: use a getter for showPropertyDialog

--- app/controllers/dashboard.js
+++ app/controllers/dashboard.js
@@ -1,14 +1,25 @@
 import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
+import { isBlank } from '@ember/utils';
 
 export default class DashboardIndexController extends Controller {
   @service intercom;
+  @service current;
 
   @action
   showMessenger() {
     this.intercom.showComposer(
       `Hi. I'm a new member and would like to add my property address to my account.`
     );
+  }
+
+  get showPropertyDialog() {
+    if(this.current.user) {
+      let property = this.current.user.get('properties.firstObject');
+      return isBlank(this.current.property || property);
+    } else {
+      return false;
+    }
   }
 }
