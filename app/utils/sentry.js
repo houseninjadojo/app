7e0dca88ecc7dad4565733217e46c406e789a01e
Miles Zimmerman <miles@houseninja.co>
2022-08-30 05:53:32 -0700

    fix(sentry): rewrite frames for debugging

--- app/utils/sentry.js
+++ app/utils/sentry.js
@@ -3,7 +3,11 @@ import * as SentryEmber from '@sentry/ember';
 import SentryRRWeb from '@sentry/rrweb';
 import config from 'houseninja/config/environment';
 import { debug } from '@ember/debug';
-import { CaptureConsole, ExtraErrorData } from '@sentry/integrations';
+import {
+  CaptureConsole,
+  ExtraErrorData,
+  RewriteFrames,
+} from '@sentry/integrations';
 // import { BrowserTracing } from '@sentry/tracing';
 import { isPresent } from '@ember/utils';
 
@@ -22,6 +26,16 @@ const integrations = [
     //   tel: true,
     //   // creditCard: true,
     // }
+  }),
+  new RewriteFrames({
+    // root path that will be stripped from the current frame's filename by the default iteratee if the filename is an absolute path
+    root: 'app:///co.houseninja.application://',
+
+    // a custom prefix that will be used by the default iteratee (default: `app://`)
+    prefix: '~',
+
+    // function that takes the frame, applies a transformation, and returns it
+    // iteratee: (frame) => frame;
   }),
 ];
 
