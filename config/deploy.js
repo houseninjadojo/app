12cbdaf2661c62ee5f39567ab92de882f6b970ba
Miles Zimmerman <miles@houseninja.co>
2022-09-06 05:41:34 -0700

    disable sandbox again

--- config/deploy.js
+++ config/deploy.js
@@ -26,23 +26,10 @@ module.exports = function (deployTarget) {
 
   if (deployTarget === 'sandbox') {
     ENV.build.environment = 'sandbox';
-    // ENV.pipeline = {
-    //   disabled: {
-    //     sentry: true,
-    //   },
-    // };
-    ENV.sentry = {
-      // the URL or CDN your js assets are served from
-      publicUrl: '~/',
-      // the sentry install you're using, https://sentry.io for hosted accounts
-      sentryUrl: 'https://sentry.io',
-      sentryOrganizationSlug: 'houseninja',
-      sentryProjectSlug: 'app',
-      revisionKey: `co.houseninja.application@${pkg.version}+${commitSHA()}`,
-      enableRevisionTagging: false,
-      sentryBearerApiKey: 'ff65161f14594302ab7597df9d8b92122cfd8029129b41b38ac13c3a05e54698', // eslint-disable-line
-
-      distDir: 'dist/assets',
+    ENV.pipeline = {
+      disabled: {
+        sentry: true,
+      },
     };
   }
 
