5d9d426f5774823bc74def20e9a6817e818bfc70
Miles Zimmerman <miles@houseninja.co>
2022-09-06 03:58:55 -0700

    restore java sentry activation

--- android/app/src/main/java/co/houseninja/app/MainActivity.java
+++ android/app/src/main/java/co/houseninja/app/MainActivity.java
@@ -10,7 +10,9 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    
-    
+    // Initializes the Bridge
+    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
+      add(SentryCapacitor.class);
+    }});
   }
 }
