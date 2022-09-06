8e76c047fb3c67b54d0fabe720d0464c0a3a8d63
Miles Zimmerman <miles@houseninja.co>
2022-09-06 03:42:08 -0700

    chore(capacitor): upgrade to capacitor 4

--- android/app/src/main/java/co/houseninja/app/MainActivity.java
+++ android/app/src/main/java/co/houseninja/app/MainActivity.java
@@ -10,9 +10,7 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      add(SentryCapacitor.class);
-    }});
+    
+    
   }
 }
