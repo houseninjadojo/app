0b6723d75b7b39d59464c97afdfc6e7cc45d863a
achillesimperial <achilles@houseninja.co>
2022-09-14 11:41:52 -0700

    primitive select on change handler update

--- app/components/primitive/select/component.js
+++ app/components/primitive/select/component.js
@@ -1,4 +1,12 @@
 import Component from '@glimmer/component';
+import { action } from '@ember/object';
 
 // eslint-disable-next-line
-export default class SelectComponent extends Component {}
+export default class SelectComponent extends Component {
+  @action
+  handleChange(e) {
+    if (this.args.onChange) {
+      this.args.onChange(e);
+    }
+  }
+}
