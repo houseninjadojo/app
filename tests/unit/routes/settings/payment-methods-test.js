d3f55347313e293622454d39f2b7384e1a21d8bc
achillesimperial <achilles@houseninja.co>
2022-09-14 14:32:26 -0700

    test fix

--- tests/unit/routes/settings/payment-methods-test.js
+++ tests/unit/routes/settings/payment-methods-test.js
@@ -1,11 +1,16 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Route | settings/payment', function (hooks) {
-  setupTest(hooks);
+module(
+  'Unit | Route | settings/payment-methods/payment-method/edit',
+  function (hooks) {
+    setupTest(hooks);
 
-  test('it exists', function (assert) {
-    let route = this.owner.lookup('route:settings/payment-methods');
-    assert.ok(route);
-  });
-});
+    test('it exists', function (assert) {
+      let route = this.owner.lookup(
+        'route:settings/payment-methods/payment-method/edit'
+      );
+      assert.ok(route);
+    });
+  }
+);
