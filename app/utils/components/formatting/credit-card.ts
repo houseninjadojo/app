21526515d987b8d74cc4f74879e08fa18bd6f9b0
achillesimperial <achilles@houseninja.co>
2022-09-20 10:11:37 -0700

    fixed max input length for card number

--- app/utils/components/formatting/credit-card.ts
+++ app/utils/components/formatting/credit-card.ts
@@ -4,29 +4,36 @@ export function formatCreditCardNumberElement(inputEl: HTMLInputElement): void {
   trackCursorPosition(inputEl, formatCardNumberElement, '-');
 }
 
-export function sanitizeNumber(num: string): string {
+function normalizeCardNumber(num: string): string {
   return num.replace(/\D/g, '');
 }
 
 function formatCardNumberElement(inputEl: HTMLInputElement) {
-  return formatCreditCardNumber(inputEl.value);
+  return formatCreditCardNumber(inputEl.value, inputEl);
 }
 
-export function formatCreditCardNumber(cardNumber: string): string {
-  const value: string = sanitizeNumber(cardNumber);
+export function formatCreditCardNumber(
+  cardNumber: string,
+  inputEl: HTMLInputElement | null = null
+): string {
+  const value: string = normalizeCardNumber(cardNumber);
   let formattedValue = '';
+  let maxLength = '';
 
   // american express, 15 digits
   if (/^3[47]\d{0,13}$/.test(value)) {
     formattedValue = value
       .replace(/(\d{4})/, '$1-')
       .replace(/(\d{4})-(\d{6})/, '$1-$2-');
+    maxLength = '17';
   } else if (/^\d{0,16}$/.test(value)) {
     // regular cc number, 16 digits
     formattedValue = value
       .replace(/(\d{4})/, '$1-')
       .replace(/(\d{4})-(\d{4})/, '$1-$2-')
       .replace(/(\d{4})-(\d{4})-(\d{4})/, '$1-$2-$3-');
+    maxLength = '19';
   }
+  inputEl?.setAttribute('maxlength', maxLength);
   return formattedValue;
 }
