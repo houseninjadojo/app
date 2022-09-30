8ba2f6fb6f19a231f2e4d30b525aa82e6ad7581c
Miles Zimmerman <miles@houseninja.co>
2022-09-29 17:38:46 -0700

    chore: pnpm

---
+++ types/mirage.d.ts
@@ -0,0 +1,9 @@
+import 'miragejs';
+
+declare module 'miragejs' {
+  export function trait(properties: Record<string, any>): any;
+  export function association(
+    trait?: string,
+    override?: Record<string, any>
+  ): any;
+}
