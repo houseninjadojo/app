e1ec8027438c3b42a61f9dfbc6716f4c7741ccb1
Tony Miller <mcfiredrill@gmail.com>
2022-10-10 12:17:23 -0700

    fix referral-code component

--- app/components/house-ninja/referral-code/component.ts
+++ app/components/house-ninja/referral-code/component.ts
@@ -12,19 +12,20 @@ export default class ServiceAreaComponent extends Component {
   @service declare router: RouterService;
   @service declare store: StoreService;
 
-  get promoCode(): string {
-    // @ts-ignore
-    return this.current.user.get('promoCode.code');
+  get promoCode(): string | undefined | null {
+    return this._promoCode();
+  }
+
+  _promoCode(): string | undefined | null {
+    return this.current.user?.get('promoCode').get('code');
   }
 
   @action
   async handleClick() {
-    // @ts-ignore
-    const promoCode = this.current.user.get('promoCode.code');
     try {
       await Share.share({
         title: 'Try House Ninja for two months FREE!', //iOS - Displays in the email subject
-        text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${promoCode} you’ll get your first two months free! www.houseninja.co/sign-up`, // iOS - Main content of email and text
+        text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${this.promoCode} you’ll get your first two months free! www.houseninja.co/sign-up`, // iOS - Main content of email and text
         // url: '', // iOS appended to main content (text property), but doesn't work
         // dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
       });
