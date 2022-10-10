0a7c6835325add210c9c9d70b48dbb008562f8d9
Tony Miller <mcfiredrill@gmail.com>
2022-10-10 10:57:11 -0700

    use CurrentService in referral-code component

--- app/components/house-ninja/referral-code/component.ts
+++ app/components/house-ninja/referral-code/component.ts
@@ -5,22 +5,26 @@ import { Share } from '@capacitor/share';
 import { captureException } from 'houseninja/utils/sentry';
 import type RouterService from '@ember/routing/router-service';
 import type StoreService from '@ember-data/store';
+import type CurrentService from 'houseninja/services/current';
 
 export default class ServiceAreaComponent extends Component {
-  @service declare current: any;
+  @service declare current: CurrentService;
   @service declare router: RouterService;
   @service declare store: StoreService;
 
-  get promoCode() {
+  get promoCode(): string {
+    // @ts-ignore
     return this.current.user.get('promoCode.code');
   }
 
   @action
   async handleClick() {
+    // @ts-ignore
+    const promoCode = this.current.user.get('promoCode.code');
     try {
       await Share.share({
         title: 'Try House Ninja for two months FREE!', //iOS - Displays in the email subject
-        text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${this.promoCode} you’ll get your first two months free! www.houseninja.co/sign-up`, // iOS - Main content of email and text
+        text: `Hi! I wanted to share a service with you that I think you’ll love: House Ninja. If you join today and use my code ${promoCode} you’ll get your first two months free! www.houseninja.co/sign-up`, // iOS - Main content of email and text
         // url: '', // iOS appended to main content (text property), but doesn't work
         // dialogTitle: 'Share with buddies', //Android only. Not sure where it presents.
       });
