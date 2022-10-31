f6eb9df384dbd20a0d3f02b6c8ca8f917322d2b0
Miles Zimmerman <miles@houseninja.co>
2022-10-31 03:47:26 -0700

    fix(capacitor): switch to @capawesome-team/capacitor-firebase

--- capacitor.config.js
+++ capacitor.config.js
@@ -69,6 +69,9 @@ const plugins = {
     iosToken: process.env.MIXPANEL_TOKEN,
     androidToken: process.env.MIXPANEL_TOKEN,
   },
+  FirebaseMessaging: {
+    presentationOptions: ['badge', 'sound', 'alert'],
+  },
 };
 
 const cordova = {};
