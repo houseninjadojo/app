d2be1eefe8d3b2f6fd0571f33822a435e9904190
Miles Zimmerman <miles@houseninja.co>
2022-11-10 12:26:01 -0800

    lint fix

--- tests/integration/modifiers/window-event-test.js
+++ tests/integration/modifiers/window-event-test.js
@@ -3,11 +3,11 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Modifier | window-event', function(hooks) {
+module('Integration | Modifier | window-event', function (hooks) {
   setupRenderingTest(hooks);
 
   // Replace this with your real tests.
-  test('it renders', async function(assert) {
+  test('it renders', async function (assert) {
     await render(hbs`<div {{window-event}}></div>`);
 
     assert.ok(true);
