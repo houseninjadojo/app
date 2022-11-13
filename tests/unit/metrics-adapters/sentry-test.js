bb1e72fb83a01bc8f323a4a715f9b76a1a9523ea
Miles Zimmerman <miles@houseninja.co>
2022-11-13 04:34:27 -0800

    fix lint

--- tests/unit/metrics-adapters/sentry-test.js
+++ tests/unit/metrics-adapters/sentry-test.js
@@ -2,11 +2,11 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import Sentry from 'houseninja/metrics-adapters/sentry';
 
-module('Unit | MetricsAdapter | sentry', function(hooks) {
+module('Unit | MetricsAdapter | sentry', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
+  test('it exists', function (assert) {
     let adapter = new Sentry({
       // TODO: Add adapter config
     });
