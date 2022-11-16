3e8f1804daa9928ea1f4dff212ce924db1ea2491
Miles Zimmerman <miles@houseninja.co>
2022-11-16 04:28:05 -0800

    chore(assets): use svg-jar

--- app/components/top-bar/content/dashboard.js
+++ app/components/top-bar/content/dashboard.js
@@ -1,5 +1,5 @@
 import Component from '@glimmer/component';
-import { tracked } from '@glimmer/tracking';
+// import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { service } from '@ember/service';
 import { NATIVE_MOBILE_ROUTE } from 'houseninja/data/enums/routes';
@@ -9,7 +9,11 @@ export default class TopBarDashboardContentComponent extends Component {
   @service intercom;
   @service view;
 
-  @tracked unreadConversationCount = 0;
+  // @tracked unreadConversationCount = 0;
+
+  get unreadConversationCount() {
+    return this.intercom.unreadConversationCount;
+  }
 
   @action
   toggleSettingsViewVisibility() {
@@ -19,12 +23,12 @@ export default class TopBarDashboardContentComponent extends Component {
     this.router.transitionTo(NATIVE_MOBILE_ROUTE.SETTINGS.INDEX);
   }
 
-  @action
-  async getUnreadConversationCount() {
-    this.unreadConversationCount =
-      await this.intercom.fetchUnreadConversationCount();
-    return this.unreadConversationCount;
-  }
+  // @action
+  // async getUnreadConversationCount() {
+  //   this.unreadConversationCount =
+  //     await this.intercom.fetchUnreadConversationCount();
+  //   return this.unreadConversationCount;
+  // }
 
   @action
   showIntercom() {
