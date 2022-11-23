2057e6a9e3be105a49f022f6ddec2f41eb649424
Miles Zimmerman <miles@houseninja.co>
2022-11-22 23:40:28 -0800

    try to fix closure

--- app/services/telemetry.ts
+++ app/services/telemetry.ts
@@ -3,23 +3,20 @@ import { datadogRum } from '@datadog/browser-rum';
 import { tracked } from '@glimmer/tracking';
 import ENV from 'houseninja/config/environment';
 
+const doNotRecord: boolean = ['sandbox', 'production'].includes(ENV.environment); // eslint-disable-line prettier/prettier
+
 export default class TelemetryService extends Service {
   @tracked isRecording = false;
 
   startRecording(): void {
-    if (this.doNotRecord) return;
+    if (doNotRecord) return;
     this.isRecording = true;
-    return datadogRum.startSessionReplayRecording();
+    datadogRum.startSessionReplayRecording();
   }
 
   stopRecording(): void {
-    if (this.doNotRecord) return;
+    if (doNotRecord) return;
     this.isRecording = false;
-    return datadogRum.stopSessionReplayRecording();
-  }
-
-  get doNotRecord(): boolean {
-    const isProduction: boolean = ['sandbox', 'production'].includes(ENV.environment); // eslint-disable-line prettier/prettier
-    return !isProduction;
+    datadogRum.stopSessionReplayRecording();
   }
 }
