1392c3bbcd1e6e84fc99c0322f9f2178d56024f0
Miles Zimmerman <miles@houseninja.co>
2022-11-22 22:12:11 -0800

    dont run in test

--- app/services/telemetry.ts
+++ app/services/telemetry.ts
@@ -1,17 +1,25 @@
 import Service from '@ember/service';
 import { datadogRum } from '@datadog/browser-rum';
 import { tracked } from '@glimmer/tracking';
+import ENV from 'houseninja/config/environment';
 
 export default class TelemetryService extends Service {
   @tracked isRecording = false;
 
   startRecording(): void {
+    if (this.doNotRecord) return;
     this.isRecording = true;
     return datadogRum.startSessionReplayRecording();
   }
 
   stopRecording(): void {
+    if (this.doNotRecord) return;
     this.isRecording = false;
     return datadogRum.stopSessionReplayRecording();
+  }
+
+  get doNotRecord(): boolean {
+    const isProduction: boolean = ['sandbox', 'production'].includes(ENV.environment); // eslint-disable-line prettier/prettier
+    return !isProduction;
   }
 }
