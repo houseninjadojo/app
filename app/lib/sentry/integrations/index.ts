67d30ef4fbe337cffd8542ebaea8f8c0d0aeebb4
Miles Zimmerman <miles@houseninja.co>
2022-11-23 12:04:59 -0800

    dont use sentry rrweb

--- app/lib/sentry/integrations/index.ts
+++ app/lib/sentry/integrations/index.ts
@@ -1,6 +1,6 @@
 import CaptureConsole from './capture-console';
 import RewriteFrames from './rewrite-frames';
-import SentryRRWeb from './sentry-rrweb';
+// import SentryRRWeb from './sentry-rrweb';
 import ExtraErrorData from './extra-error-data';
 
-export default [CaptureConsole, ExtraErrorData, SentryRRWeb, RewriteFrames];
+export default [CaptureConsole, ExtraErrorData, RewriteFrames];
