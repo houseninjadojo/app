96ab8abd53f4dfb0d7564d19bbf43a84a9de811b
Miles Zimmerman <miles@houseninja.co>
2022-12-11 12:52:35 -0800

    disable any

--- types/ember-metrics/metrics-adapters/base.d.ts
+++ types/ember-metrics/metrics-adapters/base.d.ts
@@ -1,4 +1,4 @@
-/*  */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 declare module 'ember-metrics/metrics-adapters/base' {
   export interface IdentifyOptions {
@@ -13,18 +13,18 @@ declare module 'ember-metrics/metrics-adapters/base' {
   export default class BaseAdapter {
     static supportsFastBoot: boolean;
 
-    config: unknown;
-    metrics: unknown;
+    config: any;
+    metrics: any;
 
     constructor(config: Config);
 
     toStringExtension(): string;
     init(): void;
-    identify(options: unknown): void;
-    trackEvent(options: unknown): void;
-    trackPage(options: unknown): void;
-    alias(options: unknown): void;
-    setProfile(options: unknown): void;
+    identify(options: any): void;
+    trackEvent(options: any): void;
+    trackPage(options: any): void;
+    alias(options: any): void;
+    setProfile(options: any): void;
     uninstall(): void;
     willDestroy(): void;
     toString(): string;
