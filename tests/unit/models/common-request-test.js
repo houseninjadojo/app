25deb80c6bd3d9935296073fe92988ac349f6741
Miles Zimmerman <miles@houseninja.co>
2022-12-28 02:49:28 -0800

    experiment: ai generated tests

--- tests/unit/models/common-request-test.js
+++ tests/unit/models/common-request-test.js
@@ -1,13 +1,25 @@
+import { setupTest } from '@ember/test-helpers';
 import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
-module('Unit | Model | common request', function (hooks) {
+module('Unit | Model | common request model', function (hooks) {
   setupTest(hooks);
+  setupMirage(hooks);
 
-  // Replace this with your real tests.
   test('it exists', function (assert) {
-    let store = this.owner.lookup('service:store');
-    let model = store.createRecord('common-request', {});
+    const model = this.server.create('common-request');
     assert.ok(model);
+  });
+
+  test('it has the correct attributes', function (assert) {
+    const model = this.server.create('common-request', {
+      caption: 'test caption',
+      imgURI: 'test imgURI',
+      defaultHnChatMessage: 'test defaultHnChatMessage',
+    });
+
+    assert.strictEqual(model.caption, 'test caption');
+    assert.strictEqual(model.imgURI, 'test imgURI');
+    assert.strictEqual(model.defaultHnChatMessage, 'test defaultHnChatMessage');
   });
 });
