e97a90f154e1a6e788fbd968068bb923ae7f368b
Miles Zimmerman <miles@houseninja.co>
2023-01-31 14:07:12 -0800

    chore: modern css build system

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const isProduction = () => {
+  return EmberApp.env() === 'production';
+};
+
 const webpack = require('webpack');
 const { StatsWriterPlugin } = require('webpack-stats-plugin');
 
@@ -124,7 +128,7 @@ module.exports = function (defaults) {
     // @see https://jeffjewiss.github.io/ember-cli-postcss/docs
     postcssOptions: {
       compile: {
-        enabled: true,
+        enabled: false,
         plugins: [
           PostCSSImportPlugin,
           AutoprefixerPlugin,
@@ -152,5 +156,64 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  const { Webpack } = require('@embroider/webpack');
+  return require('@embroider/compat').compatBuild(app, Webpack, {
+    staticAddonTestSupportTrees: true,
+    staticAddonTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+    splitAtRoutes: ['signup'],
+    packagerOptions: {
+      // publicAssetURL is used similarly to Ember CLI's asset fingerprint prepend option.
+      publicAssetURL: '/',
+      // Embroider lets us send our own options to the style-loader
+      cssLoaderOptions: {
+        // don't create source maps in production
+        sourceMap: isProduction() === false,
+        // enable CSS modules
+        modules: {
+          // global mode, can be either global or local
+          // we set to global mode to avoid hashing tailwind classes
+          mode: 'global',
+          // class naming template
+          localIdentName: isProduction()
+            ? '[sha512:hash:base64:5]'
+            : '[path][name]__[local]',
+        },
+      },
+      webpackConfig: {
+        module: {
+          rules: [
+            {
+              // When webpack sees an import for a CSS files
+              test: /\.css$/i,
+              exclude: /node_modules/,
+              use: [
+                {
+                  // use the PostCSS loader addon
+                  loader: 'postcss-loader',
+                  options: {
+                    sourceMap: isProduction() === false,
+                    postcssOptions: {
+                      config: './postcss.config.js',
+                    },
+                  },
+                },
+              ],
+            },
+            {
+              test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+              type: 'asset/resource',
+            },
+            {
+              test: /\.(otf|ttf)$/i,
+              type: 'asset/resource',
+            },
+          ],
+        },
+      },
+    },
+    extraPublicTrees: [],
+  });
 };
