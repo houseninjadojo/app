19390053de4a020aa7849a62569a36523fd53790
Miles Zimmerman <miles@houseninja.co>
2023-02-02 23:54:59 -0800

    stashing

--- app/services/user-activity.ts
+++ app/services/user-activity.ts
@@ -1,6 +1,11 @@
 import { default as UActivityService } from 'ember-user-activity/services/user-activity';
+import { service } from '@ember/service';
+import { default as IScrollActivityService } from 'ember-user-activity/services/scroll-activity';
 
 export default class UserActivityService extends UActivityService {
+  @service('ember-user-activity@scroll-activity')
+  declare scrollActivity: IScrollActivityService;
+
   defaultEvents = [
     'keypress',
     'mouseenter',
