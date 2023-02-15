b304069269184a9854de6b34e5e0137d2b09384a
Miles Zimmerman <miles@houseninja.co>
2023-02-15 09:18:13 -0800

    update build config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -31,102 +31,146 @@ if (process.env.BUILD_STATS) {
 }
 
 /**
+ * ember-auto-import
+ */
+const autoImport = {
+  alias: {
+    sinon: 'sinon/pkg/sinon-esm',
+  },
+};
+
+/**
+ * Babel Configuration
+ * @see https://babeljs.io/docs/en/options
+ */
+// const babel = {
+//   sourceMaps: 'inline',
+//   plugins: [
+//     ...require('ember-cli-code-coverage').buildBabelPlugin({ embroider: true }),
+//   ],
+// };
+
+/**
+ * Ember App
+ */
+// const sourcemaps = {
+//   enabled: true,
+//   extensions: ['js']
+// };
+
+/**
+ * @embroider/macros
+ */
+const emberMacros = {
+  setConfig: {
+    // Polyfill crypto.randomUUID
+    '@ember-data/store': {
+      polyfillUUID: true
+    },
+  },
+};
+
+/**
+ * Embroider Configuration
+ */
+
+// Base Config
+const embroiderBase = {
+  staticAddonTestSupportTrees: true,
+  staticAddonTrees: false,
+  staticHelpers: false,
+  staticModifiers: true,
+  staticComponents: false,
+  splitAtRoutes: [],
+};
+
+// Skip transpiling these packages during build
+const skipBabel = [
+  { package: 'qunit' },
+  { package: 'sinon' },
+  { package: 'miragejs' },
+  { package: 'lottie-web' },
+];
+
+// publicAssetURL is used similarly to Ember CLI's asset fingerprint prepend option.
+const publicAssetURL = '/';
+
+/**
+ * Embroider - Webpack Configuration
+ */
+const webpackConfig = {
+  devtool: isProdLike() ? 'hidden-source-map' : 'inline-source-map',
+  module: {
+    rules: [
+      {
+        // When webpack sees an import for a CSS files
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            // use the PostCSS loader addon
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: isProdLike() === false,
+              postcssOptions: {
+                config: './postcss.config.js',
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(otf|ttf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: webpackPlugins,
+};
+
+/**
+ * Embroider - CSS Loader Options
+ */
+const cssLoaderOptions = {
+  // dont create source maps in production
+  sourceMap: isProdLike() === false,
+  // enable CSS modules
+  modules: {
+    // global mode, can be either global or local
+    // we set to global mode to avoid hashing tailwind classes
+    mode: 'global',
+    // class naming template
+    localIdentName: isProdLike()
+      ? '[sha512:hash:base64:5]'
+      : '[path][name]__[local]',
+  },
+};
+
+/**
  * Build Options
  * @see https://cli.emberjs.com/release/advanced-use/
  */
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
-    autoImport: {
-      alias: {
-        sinon: 'sinon/pkg/sinon-esm',
-      },
-    },
-    // Babel Configuration
-    // @see https://babeljs.io/docs/en/options
-    // babel: {
-    //   sourceMaps: 'inline',
-    //   plugins: [
-    //     ...require('ember-cli-code-coverage').buildBabelPlugin({ embroider: true }),
-    //   ],
-    // },
-    // sourcemaps: {
-    //   enabled: true,
-    //   extensions: ['js']
-    // },
-    // Polyfill crypto.randomUUID
-    '@embroider/macros': {
-      setConfig: {
-        '@ember-data/store': {
-          polyfillUUID: true
-        },
-      },
-    },
+    autoImport,
+    // babel,
+    // sourcemaps,
+    '@embroider/macros': emberMacros,
   });
 
   const { Webpack } = require('@embroider/webpack');
   return require('@embroider/compat').compatBuild(app, Webpack, {
-    staticAddonTestSupportTrees: true,
-    staticAddonTrees: false,
-    staticHelpers: false,
-    staticModifiers: true,
-    staticComponents: false,
-    splitAtRoutes: [],
-    skipBabel: [
-      { package: 'qunit' },
-      { package: 'sinon' },
-      { package: 'miragejs' },
-    ],
+    ...embroiderBase,
+    skipBabel,
     packagerOptions: {
-      // publicAssetURL is used similarly to Ember CLI's asset fingerprint prepend option.
       publicAssetURL: '/',
       // Embroider lets us send our own options to the style-loader
-      cssLoaderOptions: {
-        // dont create source maps in production
-        sourceMap: isProdLike() === false,
-        // enable CSS modules
-        modules: {
-          // global mode, can be either global or local
-          // we set to global mode to avoid hashing tailwind classes
-          mode: 'global',
-          // class naming template
-          localIdentName: isProdLike()
-            ? '[sha512:hash:base64:5]'
-            : '[path][name]__[local]',
-        },
-      },
-      webpackConfig: {
-        devtool: isProdLike() ? 'hidden-source-map' : 'inline-source-map',
-        module: {
-          rules: [
-            {
-              // When webpack sees an import for a CSS files
-              test: /\.css$/i,
-              exclude: /node_modules/,
-              use: [
-                {
-                  // use the PostCSS loader addon
-                  loader: 'postcss-loader',
-                  options: {
-                    sourceMap: isProdLike() === false,
-                    postcssOptions: {
-                      config: './postcss.config.js',
-                    },
-                  },
-                },
-              ],
-            },
-            {
-              test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
-              type: 'asset/resource',
-            },
-            {
-              test: /\.(otf|ttf)$/i,
-              type: 'asset/resource',
-            },
-          ],
-        },
-        plugins: webpackPlugins,
-      },
+      cssLoaderOptions,
+      webpackConfig,
     },
     extraPublicTrees: [],
   });
